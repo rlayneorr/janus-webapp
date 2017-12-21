d46892ff2b2ab3ce9c958829362f63356b572773
StephenJHuelsman817 <sjh817@gmail.com>
2017-12-21 14:41:47 -0500

    modal working, partial dropdown menu

--- client-side/src/app/Caliber/services/trainer.service.ts
+++ client-side/src/app/Caliber/services/trainer.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Inject } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { BehaviorSubject } from 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
+import { Trainer } from '../../entities/Trainer';
 
 @Injectable()
 export class TrainerService {
@@ -28,6 +30,26 @@ export class TrainerService {
       },
       err => {
         // handle the error however you want
+      }
+    );
+  }
+
+  createTrainer(name, title, email, tier) {
+    const json = {
+        'name': name,
+        'title': title,
+        'email': email,
+        'tier': tier
+    };
+
+    this.http.post(environment.addNewTrainer, json, {withCredentials: true})
+    .subscribe(
+      resp => {
+        console.log('created a new trainer');
+        this.getAll();
+      },
+      err => {
+        console.log('err');
       }
     );
   }
--- client-side/src/app/Caliber/settings/trainers/trainers.component.ts
+++ client-side/src/app/Caliber/settings/trainers/trainers.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { TrainerService } from '../../services/trainer.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+import { Trainer } from '../../../entities/Trainer';
 
 @Component({
   selector: 'app-trainers',
   templateUrl: './trainers.component.html',
-  styleUrls: ['./trainers.component.css']
+  styleUrls: ['./trainers.component.css',
+    '../../../../../node_modules/font-awesome/css/font-awesome.css']
 })
-export class TrainersComponent implements OnInit {
+export class TrainersComponent implements OnInit, OnDestroy {
+  private trainerSubscription: Subscription;
+  trainers;
+  model = new Trainer();
 
-  constructor() { }
+ closeResult: string;
+
+  constructor(private trainerService: TrainerService,
+    private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.trainerSubscription = this.trainerService.trainers$.subscribe((resp) => {
+      this.trainers = resp;
+    });
   }
 
+  getAllTrainers() {
+    this.trainerService.getAll();
+  }
+
+  addTrainer(form) {
+    
+    console.log(this.model.name + ' '  + this.model.email + ' ' + this.model.title + ' ' + this.model.tier);
+    alert(this.model.name + ' '  + this.model.email + ' ' + this.model.title + ' ' + this.model.tier);
+    // this.trainerService.createTrainer(name, title, email, tier);
+  }
+
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // clean up subscriptions
+  ngOnDestroy() {
+    this.trainerSubscription.unsubscribe();
+  }
 }
+
