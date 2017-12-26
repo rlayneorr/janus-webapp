af6f2df42f696c450e88962a858c60513782da15
jlowell000 <jlowell000@gmail.com>
2017-12-26 14:52:58 -0500

    minor edit

--- client-side/src/app/Caliber/reports/graph/graph.component.ts
+++ client-side/src/app/Caliber/reports/graph/graph.component.ts
@@ -1,7 +1,20 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 /**
-* @author John Hudson
+ * To use this component insert this html element:
+ *<app-graph  *ngIf="chartData" [data]="(chartData | graphData:dataSetLabels)" [legend]=true [type]="chartType"></app-graph>
+ *
+ *chartData is an array of datasets. [dataset 1,dataset 2 . . .]
+ *each dataset is the raw return from the service
+ *the pipe 'graphData' in the example takes the raw data from the service and formats it.
+ *dataSetLabels is an array that is the label for each dataset
+ *
+ *chartType is the type of chart to display must string one of these strings {'line', 'radar', 'bar', 'doughnut'}
+ *
+ *Tables follow the same format.
+ *
+ *trainee-tech-skills is an example of both grpah and table in use.
+ * @author John Hudson
 */
 
 @Component({
