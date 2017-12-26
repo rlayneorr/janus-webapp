244c012740165a6be06c0a9cb17f1a940028415a
Kyle Settles <kylesettles00@gmail.com>
2017-12-26 11:12:58 -0500

    minor fixes and getting everything to build works

--- client-side/src/environments/environment.prod.ts
+++ client-side/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   context: 'http://localhost:8080/test-app/', // change for what the production environment would actually be
+  addNewCategory: 'http://localhost:8080/vp/category',
+  getAllCategories: 'http://localhost:8080/vp/category',
+  addNewTrainer: 'http://localhost:8080/vp/trainer/create',
+  editCurrentCategory: 'http://localhost:8080/vp/category/update'
 };
