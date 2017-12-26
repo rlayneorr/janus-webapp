1585068abcc60c009366e9c8f095044ae562b800
Kyle Settles <kylesettles00@gmail.com>
2017-12-26 16:00:13 -0500

    adding subject to category component

--- client-side/src/app/Caliber/settings/categories/categories.component.ts
+++ client-side/src/app/Caliber/settings/categories/categories.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Category } from '../../entities/Category';
 import { Http } from '@angular/http';
 import { environment } from '../../../../environments/environment';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-categories',
@@ -15,6 +16,7 @@ export class CategoriesComponent implements OnInit {
 
   model = new Category();
   newCategory: Category = new Category();
+  private categorySubscription: Subscription;
   categories: Category[];
   currentCategory: Category;
   isActive: boolean;
@@ -22,43 +24,33 @@ export class CategoriesComponent implements OnInit {
   constructor(private categoriesService: CategoriesService, private modalService: NgbModal, private http: Http) { }
 
   ngOnInit() {
-
-    this.http.get(environment.getAllCategories, { withCredentials: true })
-      .subscribe((succResp) => {
-        this.categories = succResp.json();
-        console.log(this.categories);
-      });
+    this.categorySubscription = this.categoriesService.categories$.subscribe((resp) => {
+      this.categories = resp;
+    });
   }
 
   addNewCategory() {
     this.newCategory.skillCategory = this.model.skillCategory;
     this.newCategory.active = true;
-    this.http.post(environment.addNewCategory, this.newCategory, { withCredentials: true })
-      .subscribe(
-      resp => {
-        console.log(resp.json());
-        this.categories.push(resp.json());
-      },
-      err => {
-        console.log(err);
-      }
-      );
+    this.categoriesService.addNewCategory(this.newCategory);
   }
 
   activeChange(activeValue) {
     console.log(activeValue);
     this.isActive = activeValue;
   }
+
   editCurrentCategory() {
     this.currentCategory.active = this.isActive;
     this.categoriesService.editCurrentCategory(this.currentCategory);
-
   }
+
   open(content) {
     this.modalService.open(content).result.then((result) => {
     }, (reason) => {
     });
   }
+
   editopen(content, index: Category) {
     this.currentCategory = index;
     this.isActive = index.active;
