d69a48832c4462ae4c32ac5eab3ab17aad55e9a0
Kelton Keener <keltonkeener@gmail.com>
2017-12-27 16:23:03 -0500

    working in revaturions

--- client-side/src/app/Caliber/assess/assess.component.ts
+++ client-side/src/app/Caliber/assess/assess.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule} from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
+import { BatchService } from '../services/batch.service';
+import { HttpClientModule  } from '@angular/common/http';
+import { Batch } from '../entities/Batch';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Assessment } from '../entities/Assessment';
+import { AssessmentService } from '../services/assessment.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GradeService } from '../services/grade.service';
+import { Grade } from '../entities/Grade';
+import { Trainee } from '../entities/Trainee';
+
 
 @Component({
   selector: 'app-assess',
@@ -7,9 +19,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssessComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string;
+  register= {
+    first_name: '',
+    last_name: '',
+    username: '',
+    password: '',
+    email: '',
+  };
+
+  assessment: Assessment;
+  loading = false;
+
+  batches: Batch[] = [];
+  assessments: Assessment[] = [];
+  selectedBatch: Batch = new Batch();
+  grades: Grade[] = [];
+
+  constructor(private modalService: NgbModal, private batchService: BatchService, private assessmentService: AssessmentService,
+  private gradeService: GradeService) {
+
+  }
 
   ngOnInit() {
+    this.batchService.fetchAll();
+    this.batchService.getList().subscribe(batch => this.batches = batch);
+    this.assessmentService.getList().subscribe(assessment => this.assessments = assessment);
+    this.gradeService.getList().subscribe(grade => this.grades = grade);
+
+  }
+
+  open(content) {
+    // this.modalService.open(content);
+    this.selectedBatch = this.batches[0];
+    this.assessmentService.fetchByBatchIdByWeek(this.selectedBatch.batchId, 1);
+    this.gradeService.fetchByBatchIdByWeek(this.selectedBatch.batchId, 1);
+
+
+  }
+
+  getAssessments(week: number) {
+    this.assessmentService.fetchByBatchIdByWeek(this.selectedBatch.batchId, week);
+  }
+
+  getGrade(traineeId, assessmentId) {
+
+    this.grades.forEach(grade => {
+
+        if (grade.trainee.traineeId === traineeId && grade.assessment.assessmentId === assessmentId) {
+          console.log('success ' + grade.score);
+          return grade.score;
+        }
+    });
+
+    
+  }
+
+  counter(i: number) {
+    return new Array(i);
   }
 
 }
