d50c5ec1242d017e61d5ac602856ad2603ae55f7
Kyle Settles <kylesettles00@gmail.com>
2017-12-28 10:28:41 -0500

    refactoring the categories service to use HttpClient

--- client-side/src/app/Caliber/services/categories.service.ts
+++ client-side/src/app/Caliber/services/categories.service.ts
@@ -1,10 +1,20 @@
-import { Injectable, Inject } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// rxjs
 import { BehaviorSubject } from 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
+
+// services
 import { environment } from '../../../environments/environment';
+
+// entities
 import { Category } from '../entities/Category';
 
+/**
+ * this service is used to manage API calls
+ * for the category objects
+ */
 @Injectable()
 export class CategoriesService {
 
@@ -13,48 +23,42 @@ export class CategoriesService {
 
   categories$: Observable<any> = this.dataSubject.asObservable(); // this is how components should access the data if you want to cache it
 
-  constructor(@Inject(Http) public http: Http) {
-    this.getAll();
+  constructor(private http: HttpClient) {
+    this.fetchAll();
   }
 
-  getAll(): void {
-    this.http.get(environment.getAllCategories, {withCredentials: true})
-    .map(
-      resp => resp.json(), // map the resp so all subscribers just get the body of the request as a js object
-      // err => // can have the error mapped for all subscribers if you want also
-    )
-    .subscribe(
-      resp => {
-        this.dataSubject.next(resp);
-      },
-      err => {
-        // handle the error however you want
-      }
-    );
+  /**
+   * retrieves a list of all categories and pushes them to the
+   * data subject
+   *
+   */
+  public fetchAll(): void {
+    this.http.get<Category[]>(environment.getAllCategories).subscribe( (resp) => {
+      this.dataSubject.next(resp);
+    });
   }
 
-  // adds a new category to the database
-  addNewCategory(newCategory) {
-    this.http.post(environment.addNewCategory, newCategory, {withCredentials: true})
-    .subscribe(
-      resp => {
-        this.getAll();
-      },
-      err => {
-        console.log(err);
-      }
-    );
+  /**
+   * Creates a new category and pushes it on the same
+   * data subject
+   *
+   * @param newCategory
+   */
+  public addNewCategory(newCategory: Category): void {
+    const data = JSON.stringify(newCategory);
+    this.http.post<Category>(environment.addNewCategory, data).subscribe( (resp) => {
+      this.fetchAll();
+    });
   }
 
-  editCurrentCategory(currentCategory: Category) {
-    this.http.put(environment.editCurrentCategory, currentCategory, {withCredentials: true})
-    .subscribe(
-      resp => {
-        console.log(resp.json());
-      },
-      err => {
-        console.log(err);
-      }
-    );
+  /**
+   * edits a the currently selected category and
+   * updates the category in the database
+   *
+   * @param currentCategory
+   */
+  public editCurrentCategory(currentCategory: Category): void {
+    const data = JSON.stringify(currentCategory);
+    this.http.put(environment.editCurrentCategory, data).subscribe( (resp) => { });
   }
 }
