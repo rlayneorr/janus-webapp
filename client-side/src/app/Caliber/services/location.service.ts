b75fffddee759d56f0cc134d7256d0d529de6535
randythiencao <randythiencao@yahoo.com>
2017-12-28 09:51:43 -0500

    converted httpclient

--- client-side/src/app/Caliber/services/location.service.ts
+++ client-side/src/app/Caliber/services/location.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
 import { Location } from '../../entities/Location';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class LocationService {
@@ -12,67 +13,49 @@ export class LocationService {
 
   locations$: Observable<any> = this.dataSubject.asObservable(); // this is how components should access the data if you want to cache it
 
-  constructor( @Inject(Http) public http: Http) {
+  constructor(private http: HttpClient) {
   }
 
   getAll() {
-    this.http.get(environment.getAllLocations, { withCredentials: true })
-    .map(
-      resp => resp.json(), // map the resp so all subscribers just get the body of the request as a js object
-      // err => // can have the error mapped for all subscribers if you want also
-    )
-      .subscribe(
-      resp => {
-        this.dataSubject.next(resp);
-      },
-      err => {
-        // handle the error however you want
-      }
-      );
+    this.http.get<Location[]>(environment.getAllLocations).subscribe((resp) => {
+      console.log('got all locations');
+      this.dataSubject.next(resp);
+    },
+      (err) => {
+        console.log('error fetching all locations');
+      });
   }
 
   addLocation(location: Location) {
-    this.http.post(environment.addLocation, location, { withCredentials: true })
-      .map(
-      resp => resp.json(),
-    )
-      .subscribe(
-      resp => {
-        console.log('added location successfully');
-        this.getAll();
-      },
-      err => {
-        console.log('err adding location ' + err);
-      }
-      );
+    this.http.post(environment.addLocation, location).subscribe((resp) => {
+      console.log('added a location');
+      this.getAll();
+    },
+      (err) => {
+        console.log('error adding location');
+      });
   }
 
 
   updateLocation(location: Location) {
-    this.http.put(environment.editLocation, location, { withCredentials: true })
-      .map(
-      resp => resp.json(),
-    )
-      .subscribe(
-      resp => {
-        console.log('updated location successfully');
-        this.getAll();
-      },
-      err => {
-        console.log('err updating location ' + err);
-      }
-      );
+    this.http.put(environment.editLocation, location).subscribe((resp) => {
+      console.log('edited a location');
+      this.getAll();
+    },
+      (err) => {
+        console.log('error editing location');
+      });
   }
 
-  deleteLocation(location: Location) {
-    location.active = false;
-    this.http.delete(environment.deleteLocation, { withCredentials: true, body: location })
-      .subscribe(
-      resp => {
-      },
-      err => {
-        // handle the error however you want
-      }
-      );
-  }
+  // deleteLocation(location: Location) {
+  //   location.active = false;
+  //   this.http.delete(environment.deleteLocation, { withCredentials: true, body: location })
+  //     .subscribe(
+  //     resp => {
+  //     },
+  //     err => {
+  //       // handle the error however you want
+  //     }
+  //     );
+  // }
 }
