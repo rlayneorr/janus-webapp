020ec1ab69343023f093b9492383af23de89cf98
Kelton Keener <keltonkeener@gmail.com>
2017-12-29 09:18:52 -0500

    a

--- client-side/src/app/Caliber/assess/assess.component.ts
+++ client-side/src/app/Caliber/assess/assess.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgModule} from '@angular/core';
+import { Component, OnInit, NgModule, ViewEncapsulation} from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { BatchService } from '../services/batch.service';
 import { HttpClientModule  } from '@angular/common/http';
@@ -15,11 +15,10 @@ import { Trainee } from '../entities/Trainee';
 @Component({
   selector: 'app-assess',
   templateUrl: './assess.component.html',
-  styleUrls: ['./assess.component.css']
+  styleUrls: ['./assess.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AssessComponent implements OnInit {
-
-
 
   assessment: Assessment;
   loading = false;
@@ -70,10 +69,16 @@ export class AssessComponent implements OnInit {
     this.batchService.update(this.selectedBatch);
   }
 
+  addAssessment(type, category, score) {
+    const newAssess = new Assessment();
+    newAssess.category = category;
+    newAssess.type = type;
+    newAssess.rawScore = score;
+  }
+
   getGrade(traineeId, assessmentId) {
     this.grades.forEach(grade => {
       if (grade.trainee.traineeId === traineeId && grade.assessment.assessmentId === assessmentId) {
-
         return grade.score;
       }
     });
