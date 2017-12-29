1ef670a0f804980ea58c0d1b048cf53bbe49cf0d
Micah West <micahwest13@gmail.com>
2017-12-29 14:53:10 -0500

    Completed individual trainee panel feedback component.

--- client-side/src/app/Caliber/entities/Panel.ts
+++ client-side/src/app/Caliber/entities/Panel.ts
@@ -1,6 +1,8 @@
 import { Trainer } from './Trainer';
 import { Trainee } from './Trainee';
 
+// When making merges, please use the version presented by the Revaturions group.
+
 export class Panel {
     panelId: number;
     trainee: Trainee;
@@ -19,6 +21,5 @@ export class Panel {
     projectThreeDescription: string;
     communicationSkills: string;
     overall: string;
-    // REPORTS GROUP: REMOVE ON MERGE
     feedback: Array<any>;
 }
