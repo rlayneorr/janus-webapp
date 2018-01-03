0fc623924b6d9ceab5e2006536bbc12f5c11ca87
randythiencao <randythiencao@yahoo.com>
2018-01-03 10:39:59 -0500

    notif for trainer/locations

--- client-side/src/app/Caliber/services/alerts.service.ts
+++ client-side/src/app/Caliber/services/alerts.service.ts
@@ -10,14 +10,25 @@ export class AlertsService {
   constructor() {
   }
 
-  success(message: string, keepAfterNavigationChange = false) {
+  /**
+   * push message type and message content into subject to broadcaast
+   * @param message success message
+   */
+  success(message: string) {
     this.subject.next({ type: 'success', text: message });
   }
 
-  error(message: string, keepAfterNavigationChange = false) {
+  /**
+ * push message type and message content into subject to broadcaast
+ * @param message success message
+ */
+  error(message: string) {
     this.subject.next({ type: 'error', text: message });
   }
 
+  /**
+   * return subject as observable for subscribers
+   */
   getMessage(): Observable<any> {
     return this.subject.asObservable();
   }
