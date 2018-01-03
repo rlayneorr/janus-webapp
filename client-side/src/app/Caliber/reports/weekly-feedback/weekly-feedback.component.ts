4d8092b4d47e72b82c4dfd348832d2a748bab618
Micah West <micahwest13@gmail.com>
2018-01-03 09:53:23 -0500

    Added weekly feedback component.

--- client-side/src/app/Caliber/reports/weekly-feedback/weekly-feedback.component.ts
+++ client-side/src/app/Caliber/reports/weekly-feedback/weekly-feedback.component.ts
@@ -5,6 +5,16 @@ import { Subscription } from 'rxjs/Subscription';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 
+// entities
+import { Trainee } from '../../entities/Trainee';
+import { Batch } from '../../entities/Batch';
+import { Note } from '../../entities/Note';
+
+// services
+import { GranularityService } from '../services/granularity.service';
+import { EvaluationService } from '../../../services/evaluation.service';
+import { ReportingService } from '../../../services/reporting.service';
+
 @Component({
   selector: 'app-weekly-feedback',
   templateUrl: './weekly-feedback.component.html',
@@ -12,14 +22,81 @@ import { Observable } from 'rxjs/Observable';
 })
 export class WeeklyFeedbackComponent implements OnInit, OnDestroy {
 
+  private qcNoteSubscription: Subscription;
+  private trainerNoteSubscription: Subscription;
 
+  private traineeSubscription: Subscription;
+  private batchSubscription: Subscription;
+  private weekSubscription: Subscription;
+  private topicSubscription: Subscription;
 
-  constructor() { }
+  week = 1;
+  trainee: Trainee;
+  batch: Batch;
+
+  weekTopics: string;
+  qcNote: Note;
+  trainerNote: Note;
+
+  constructor(private granularityService: GranularityService,
+              private evalService: EvaluationService,
+              private reportService: ReportingService) { }
 
   ngOnInit() {
+
+    this.traineeSubscription = Observable.combineLatest(
+        this.granularityService.currentTrainee$, this.granularityService.currentWeek$).subscribe((traineeWeek) => {
+
+        this.week = traineeWeek[1];
+        this.evalService.fetchQCTraineeNote(traineeWeek[0].traineeId, traineeWeek[1]);
+        this.evalService.fetchTraineeNote(traineeWeek[0].traineeId, traineeWeek[1]);
+    });
+
+    this.traineeSubscription = Observable.combineLatest(
+      this.granularityService.currentBatch$, this.granularityService.currentWeek$).subscribe((batchWeek) => {
+
+      this.reportService.fetchTechnologiesForTheWeek(batchWeek[0].batchId, batchWeek[1]);
+    });
+
+    this.qcNoteSubscription = this.evalService.qcTraineeNote$.subscribe((note) => {
+      if (note) {
+        this.qcNote = note.data;
+      }
+    });
+
+    this.trainerNoteSubscription = this.evalService.traineeNote$.subscribe((note) => {
+      if (note) {
+        this.trainerNote = note.data;
+      }
+    });
+
+    this.topicSubscription = this.reportService.technologiesForTheWeek$.subscribe((tech) => {
+      if (tech) {
+        this.weekTopics = this.techString(tech.data);
+      }
+    });
+  }
+
+  techString(tech: Array<String>) {
+    let result = '';
+
+    for (let i = 0; i < tech.length; i++) {
+      result += tech[i];
+      if (i < tech.length - 1) {
+        result += ', ';
+      }
+    }
+
+    return result;
   }
 
   ngOnDestroy() {
+    this.qcNoteSubscription.unsubscribe();
+    this.trainerNoteSubscription.unsubscribe();
+    this.traineeSubscription.unsubscribe();
+    this.batchSubscription.unsubscribe();
+    this.weekSubscription.unsubscribe();
+    this.topicSubscription.unsubscribe();
   }
 
 }
