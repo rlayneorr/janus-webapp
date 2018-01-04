9f39110a836ec28b72acd0377a853187a73e5d83
Micah West <micahwest13@gmail.com>
2018-01-04 09:42:11 -0500

    Import OnDestroy from @angular/core.
    This brings the import in line with other lifecycle hooks. You import OnInit from @angular/core, you should import all lifecycle hooks from there too.

--- client-side/src/app/Caliber/reports/panel-batch-all-trainees/panel-batch-all-trainees.component.ts
+++ client-side/src/app/Caliber/reports/panel-batch-all-trainees/panel-batch-all-trainees.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PanelReview } from '../../entities/PanelReview';
 import { Subscription } from 'rxjs/Subscription';
 import { ReportingService } from '../../../services/reporting.service';
 import { GranularityService } from '../services/granularity.service';
-import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
-
 
 /**
  * Component utilizes service API calls to fetch and display the results of
