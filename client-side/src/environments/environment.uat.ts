d462236f0ef0318cee25155aeabd1cc46bdb38d1
Blake Kruppa <blake.kruppa@wallawalla.edu>
2018-02-03 23:11:25 -0500

    Update environment.uat.ts

--- client-side/src/environments/environment.uat.ts
+++ client-side/src/environments/environment.uat.ts
@@ -2,7 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-const context = 'http://ec2-18-216-169-252.us-east-2.compute.amazonaws.com:8080/';
+const context = 'https://dev-caliber.revature.tech/';
 export const environment = {
   production: false,
   context: context, // change for what the production environment would actually be
