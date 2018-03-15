3133121f1497aba639d2e720216fe9a55ef1a7d7
Conan Chan <chanconan67@gmail.com>
2018-03-15 18:17:31 -0400

    added all services files, modified module to reflect it

--- client-side/src/app/portals/Caliber/caliber.module.ts
+++ client-side/src/app/portals/Caliber/caliber.module.ts
@@ -44,6 +44,12 @@ import { EvaluationService } from './services/evaluation.service';
 import { QCStatusService } from './services/qcstatus.service';
 import { TraineeStatusService } from './services/trainee-status.service';
 
+//1801-caliber-dev-angels services
+import { QuestionsService } from './settings/screening/services/questions.service';
+import { TracksService } from './settings/screening/services/tracks.service';
+import { BucketsService } from './settings/screening/services/buckets.service';
+import { TagsService } from './settings/screening/services/tags.service';
+
 //N.T.
 import { ApiService } from './util/api.service';
 
@@ -249,7 +255,11 @@ import { ScreeningComponent } from './settings/screening/screening.component';
     ReportsService,
     QCStatusService,
     TraineeStatusService,
-    ApiService
+    ApiService,
+    QuestionsService,
+    TracksService,
+    BucketsService,
+    TagsService,
   ],
   bootstrap: [
     TrainersComponent
