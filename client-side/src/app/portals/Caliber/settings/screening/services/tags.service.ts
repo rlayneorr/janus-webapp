8ba2dea0e1fa3e45d69e75e1dc238ce1dd83ba6d
Conan Chan <chanconan67@gmail.com>
2018-03-15 20:05:04 -0400

    Added all the URLs for service calls

--- client-side/src/app/portals/Caliber/settings/screening/services/tags.service.ts
+++ client-side/src/app/portals/Caliber/settings/screening/services/tags.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+    })
+};
 
 @Injectable()
 export class TagsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  url: string = "/tag/";
+
+  getAllTags(){
+      return this.http.get(this.url + "getTags");
+  }
+
+  createNewTag(name: string){
+      return this.http.post(this.url + "createNewTag", name, httpOptions);
+  }
 }
--- client-side/src/app/portals/Caliber/settings/screening/services/tracks.service.ts
+++ client-side/src/app/portals/Caliber/settings/screening/services/tracks.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+    })
+};
 
 @Injectable()
 export class TracksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url: string = "/track/";
+
+  //Buckets will probably be an array of buckets after model/entity is created
+  createTrack(description: string, buckets: string[], weights){
+    return this.url.post(this.url + "createTrack", {description: description, buckets: buckets, weights: weights}, httpOptions);
+  }
+
+  deactivateTrack(trackId: int){
+      return this.url.put(this.url + "deactivateTrack", trackId, httpOptions);
+  }
+
+  activateTrack(trackId: int){
+      return this.url.put(this.url + "activateTrack", trackId, httpOptions);
+  }
+
+  getTracks(){
+      return this.url.get(this.url + "getTracks");
+  }
+
+  updateTrack(trackId: int){
+      return this.url.put(this.url + "updateTrack", trackId, httpOptions);
+  }
+
+  getTrackById(trackId: int){
+      return this.url.get(this.url + "getTrack/" + trackId);
+  }
+
+  getBucketsByTrack(trackId: int){
+      return this.url.get(this.url + "getTopics/" + trackId);
+  }
 
 }
