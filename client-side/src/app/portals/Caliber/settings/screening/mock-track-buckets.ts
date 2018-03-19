ec6f737c0eaa418cb35b661bf13ef1bf9acfa668
Omar Mohamad <omarbmohamad@gmail.com>
2018-03-19 12:00:26 -0400

    mock data added

---
+++ client-side/src/app/portals/Caliber/settings/screening/mock-track-buckets.ts
@@ -0,0 +1,9 @@
+import {TrackBucket} from './entities/TrackBucket';
+
+export const TRACKBUCKETS: TrackBucket[]=[
+    {trackId: 1, bucketId: 1, weight: 20},
+    {trackId: 1, bucketId: 2, weight: 20},
+    {trackId: 1, bucketId: 3, weight: 30},
+    {trackId: 1, bucketId: 4, weight: 30}
+
+]
--- client-side/src/app/portals/Caliber/settings/screening/track-buckets/track-buckets.component.ts
+++ client-side/src/app/portals/Caliber/settings/screening/track-buckets/track-buckets.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit,Input } from '@angular/core';
+import {Router} from '@angular/router';
+import {TrackBucket} from '../entities/TrackBucket';
+import {BucketsService} from '../services/buckets.service';
+import {Track} from '../entities/Track';
+import { TracksService } from '../services/tracks.service';
+
 
 @Component({
   selector: 'app-track-buckets',
   templateUrl: './track-buckets.component.html',
-  styleUrls: ['./track-buckets.component.css']
+  styleUrls: ['./track-buckets.component.css'],
 })
-export class TrackBucketsComponent implements OnInit {
+export class TrackBucketsComponent 
+implements OnInit {
+  @Input() track: Track;
+  public allTrackBuckets: any[]=[];
+  public trackBuckets=[];
 
-  public buckets: any[]=[];
-  public activeBuckets: any[]=[];
 
-  constructor() { }
+  constructor(
+    private tracksService: TracksService, 
+    private route: ActivatedRoute,
+  private location: Location) { }
 
   ngOnInit() {
-    this.activeBuckets=[
-      {Name:"OOP"},
-      {Name:"Spring"}
+    //this.track={id:1,name:"Java",isActive:true};
+   
+    this.getTrack();
+    //this.setTrackBuckets()
 
+    this.allTrackBuckets=[
+   
     ]
   }
+ 
+  getTrack(): void{
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.tracksService.getTrackById(id).subscribe( track => this.track=track);
+  }
+  
 
 }
