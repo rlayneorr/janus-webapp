6861c9fc4dcb98d35c985e5cd91279b506f5d96a
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-03-19 22:21:41 -0400

    Created http-error-handler service; added imports in caliber.module.ts; added entities needed in track-buckets.components.ts; removed 'public' access modifiers in tracks

--- client-side/src/app/portals/Caliber/settings/screening/services/buckets.service.ts
+++ client-side/src/app/portals/Caliber/settings/screening/services/buckets.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Bucket } from '../entities/Bucket';
+
 const httpOptions = {
 headers: new HttpHeaders({
         'Content-Type':  'application/json',
