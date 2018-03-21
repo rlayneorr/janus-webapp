4db57857d1c609ea32ee1ef6b0331f9f39540072
genome3po <jeffrey.rubi.885@my.csun.edu>
2018-03-20 21:34:27 -0400

    updated mock-bucket.ts; committing to pull

--- client-side/src/app/portals/Caliber/settings/screening/mock-bucket.ts
+++ client-side/src/app/portals/Caliber/settings/screening/mock-bucket.ts
@@ -6,8 +6,8 @@
     import {Bucket} from './entities/Bucket';
 
     export const BUCKETS: Bucket[]=[
-        {id:1, category:"OOP", description:"OOP Intro", isActive:true},
-        {id:2, category:"OOP", description:"OOP Intro", isActive:true},
-        {id:3, category:"OOP", description:"OOP Intro", isActive:true},
-        {id:4, category:"OOP", description:"OOP Intro", isActive:true}
+        {id:1, name:"OOP", description:"OOP Intro"},
+        {id:2, name:"OOP", description:"OOP Intro"},
+        {id:3, name:"OOP", description:"OOP Intro"},
+        {id:4, name:"OOP", description:"OOP Intro"}
     ]
