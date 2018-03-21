b2163e1fae0a4cdd07e0c738651f4d8437d2f6ac
Sleepy122 <Calder.Jones3155@gmail.com>
2018-03-21 11:11:53 -0400

    changes

--- client-side/src/app/portals/Caliber/settings/screening/mock-bucket.ts
+++ client-side/src/app/portals/Caliber/settings/screening/mock-bucket.ts
@@ -3,7 +3,6 @@
     description: string;
     isActive: boolean;*/
 
-<<<<<<< HEAD
     import {Bucket} from './entities/Bucket';
 /*
     export const BUCKETS: Bucket[]=[
@@ -13,14 +12,4 @@
         {id:4, name:"OOP", description:"OOP Intro"}
     ]
     */
-=======
-import {Bucket} from './entities/Bucket';
-
-export const BUCKETS: Bucket[] = [
-    {id: 1, name: "OOP", description: "OOP Intro"},
-    {id: 2, name: "OOP", description: "OOP Intro"},
-    {id: 3, name: "OOP", description: "OOP Intro"},
-    {id: 4, name: "OOP", description: "OOP Intro"}
-]
->>>>>>> 8c30649606ab70606998713853fac7513cfb2f4e
 
