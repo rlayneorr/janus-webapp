dbe53a0f8f1954e97f63fcd28a99e37eeda056d2
Christian Diaz <cdiaz8369@hotmail.com>
2018-03-24 19:29:22 -0400

    Created Protractor some protractor test (Christian) o	Edited app.e2e-spec.ts to add some test. (Christian) o	Edited app.po.ts to add some methods for tests. (Christian)
    -	Added Ids to navbar options for TrackForce (Christian)
    o	Edited track-force-nav.component.html to add ids to navbar options. (Christian)
    o	Edited nav.component.html to add ids to navbar options. (Christian)

--- client-side/e2e/app.e2e-spec.ts
+++ client-side/e2e/app.e2e-spec.ts
@@ -2,13 +2,43 @@ import { AppPage } from './app.po';
 
 describe('test-app App', () => {
   let page: AppPage;
-
+  
   beforeEach(() => {
     page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitleText()).toEqual('Please Select One Of the Following Applications :');
   });
+
+  it('should display Assign force button', () => {
+    expect(page.getAssignForceTitleText()).toEqual('Assign Force');
+  });
+
+  it('should display Caliber button', () => {
+    expect(page.getCaliberTitleText()).toEqual('Caliber');
+  });
+
+ it('should display Track Force button', () => {
+    expect(page.getTrackForceTitleText()).toEqual('Track Force');
+  });
+
+  it('should click Tracforce button and go to trackfroce login in page', () => {
+    page.clickTrackForceButton();
+    expect(page.getTrackForceLoginTitleText()).toEqual('Track Force Login');
+  });
+
+  it('should log in to Track Force', () => {
+    page.sendTrackForceLogin();
+    expect(page.getTrackForceLoggedInTitleText()).toEqual('Displaying SalesForce Statistics');
+  });
+
+it('should go Client List Page', () => {
+    page.goToClientListPage();
+    expect(page.getClientListTitleText()).toEqual('Show Clients With No Associates');
+  });
+
+  
+
 });
