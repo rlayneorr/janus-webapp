d76e2b492c92e3cb89bb5f2e6f61a0d02f24847c
nathan <nathanblanchard12@gmail.com>
2018-03-27 18:39:33 -0400

    added functional graph

--- client-side/src/app/portals/Track-Force/components/client-list/client-list.component.ts
+++ client-side/src/app/portals/Track-Force/components/client-list/client-list.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { RequestService } from '../../services/request-service/request.service';
-import { ClientListService } from '../../services/client-list-service/client-list.service';
-import { Subject } from 'rxjs/Subject';
-import { Client } from '../../models/client.model';
-import { Observable } from 'rxjs/Observable';
-import { SelectedStatusConstants } from '../../constants/selected-status.constants';
-import { ThemeConstants } from '../../constants/theme.constants';
-import { Color } from 'ng2-charts';
-import { StatusInfo } from '../../models/status-info.model';
-import {MarketStatusService} from "../../services/market-status/market-status.service";
-import {AssociateService} from "../../services/associates-service/associates-service";
-import {InterviewService} from "../../services/interview-service/interview-service";
-import {PlacementService} from "../../services/placement-service/placement.service";
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {RequestService} from '../../services/request-service/request.service';
+import {ClientListService} from '../../services/client-list-service/client-list.service';
+import {Subject} from 'rxjs/Subject';
+import {Client} from '../../models/client.model';
+import {Observable} from 'rxjs/Observable';
+import {SelectedStatusConstants} from '../../constants/selected-status.constants';
+import {ThemeConstants} from '../../constants/theme.constants';
+import {Color} from 'ng2-charts';
+import {StatusInfo} from '../../models/status-info.model';
+import {MarketStatusService} from '../../services/market-status/market-status.service';
+import {AssociateService} from '../../services/associates-service/associates-service';
+import {InterviewService} from '../../services/interview-service/interview-service';
+import {PlacementService} from '../../services/placement-service/placement.service';
 
 /**
  * @author Han Jung
@@ -41,7 +41,7 @@ export class ClientListComponent implements OnInit {
     position: 'right',
     scaleShowVerticalLines: false,
     responsive: true,
-    legend: { position: 'right' },
+    legend: {position: 'right'},
     scales: {
       yAxes: [
         {
@@ -65,17 +65,15 @@ export class ClientListComponent implements OnInit {
     }
   };
   // data values initialize to 1 for animation
-  public barChartData: any[] = [{ data: [0, 0, 0, 0], label: 'Mapped' }, { data: [0, 0, 0, 0], label: 'Unmapped' }];
+  public barChartData: any[] = [{data: [0, 0, 0, 0], label: 'Mapped'}, {data: [0, 0, 0, 0], label: 'Unmapped'}];
 
 
-  constructor(
-    private rs: RequestService,
-    private clientService: ClientListService,
-    private marketStatus: MarketStatusService,
-    private associates: AssociateService,
-    private interviews: InterviewService,
-    private placement: PlacementService,
-  ) {
+  constructor(private rs: RequestService,
+              private clientService: ClientListService,
+              private marketStatus: MarketStatusService,
+              private associates: AssociateService,
+              private interviews: InterviewService,
+              private placement: PlacementService,) {
   }
 
   ngOnInit() {
@@ -85,26 +83,60 @@ export class ClientListComponent implements OnInit {
   // get client names from data and push to clientNames string array
   getAllClients() {
     let self = this;
+    this.clientInfo = [];
+    this.clientService.getAllClients().subscribe(clients => {
+      this.associates.getAllAssociates().subscribe(associates => {
+        // console.log(clients);
+        // console.log(associates);
 
-    this.marketStatus.getAllMarketingStatusMapped().subscribe(items => {
-      console.log(items);
+        clients.forEach(client => {
+          let a = associates.filter(items => items['clientId'] === client['clientId']);
+          a.forEach(items => {
+            this.marketStatus.getMarketingStatusById(items['marketingStatusId']).subscribe(status => {
+              console.log(status);
+              const tempClient: Client = {
+                id: client['clientId'],
+                name: client['clientName'],
+                tfClientName: client['clientName'],
+                placements: null,
+                associates: null,
+                interviews: null,
+                stats: status,
+              };
+              // console.log('--------');
+              // console.log(tempClient);
+              this.clientInfo.push(tempClient);
+              this.clientNames.push(tempClient.tfClientName);
+            });
+          });
+        });
+      });
     });
 
-    this.placement.getAllPlacements().subscribe(items => {
-      console.log(items);
-    });
+    setTimeout(() => {
+      this.initChartData();
+    }, 4000);
 
-    this.interviews.getAllInterviews().subscribe(items => {
-      console.log(items);
-    });
 
-    this.marketStatus.getAllMarketingStatus().subscribe(items => {
-      console.log(items);
-    });
-
-    this.associates.getAllAssociates().subscribe(items => {
-      console.log(items);
-    });
+    // this.marketStatus.getAllMarketingStatusMapped().subscribe(items => {
+    //   console.log(items);
+    // });
+    //
+    // this.placement.getAllPlacements().subscribe(items => {
+    //   console.log(items);
+    // });
+    //
+    // this.interviews.getAllInterviews().subscribe(items => {
+    //   console.log(items);
+    // });
+    //
+    // this.marketStatus.getAllMarketingStatus().subscribe(items => {
+    //   console.log(items);
+    // });
+    //
+    // this.associates.getAllAssociates().subscribe(items => {
+    //   console.log(items);
+    // });
 
     // this.clientService.getAllClients().subscribe(
     //   clients => {
@@ -132,11 +164,6 @@ export class ClientListComponent implements OnInit {
     //   }, err => {
     //     console.error("Failed grabbing names");
     //   });
-
-    this.clientService.getAllClients().subscribe(
-      clients => {
-          console.log(clients);
-      });
   }
 
   initChartData() {
@@ -150,18 +177,35 @@ export class ClientListComponent implements OnInit {
     let openUnmapped = 0;
     let selectedUnmapped = 0;
     let confirmedUnmapped = 0;
-    for (let i=0;i<this.clientInfo.length;i++) {
-      let client: Client = this.clientInfo[i];
-      let stats: StatusInfo = client.stats;
-      trainingMapped += stats.trainingMapped;
-      reservedMapped += stats.reservedMapped;
-      selectedMapped += stats.selectedMapped;
-      confirmedMapped += stats.confirmedMapped;
-      trainingUnmapped += stats.trainingMapped;
-      openUnmapped += stats.openUnmapped;
-      selectedUnmapped += stats.selectedUnmapped;
-      confirmedUnmapped += stats.confirmedUnmapped;
+    // for (let i=0;i<this.clientInfo.length;i++) {
+    //   let client: Client = this.clientInfo[i];
+    //   let stats: StatusInfo = client.stats;
+    //   trainingMapped += stats.trainingMapped;
+    //   reservedMapped += stats.reservedMapped;
+    //   selectedMapped += stats.selectedMapped;
+    //   confirmedMapped += stats.confirmedMapped;
+    //   trainingUnmapped += stats.trainingMapped;
+    //   openUnmapped += stats.openUnmapped;
+    //   selectedUnmapped += stats.selectedUnmapped;
+    //   confirmedUnmapped += stats.confirmedUnmapped;
+    // }
+
+    for(let i = 0; i < this.clientInfo.length; i++)
+    {
+      console.log(this.clientInfo[i]['stats']['marketingStatusName']);
+      let status = this.clientInfo[i]['stats']['marketingStatusName'].toLowerCase();
+      if(status.includes('unmapped')) {
+        console.log('unmapped');
+        if(status.includes('training')){
+          trainingUnmapped++;
+        }
+      }else{
+        if(status.includes('training')){
+          trainingMapped++;
+        }
+      }
     }
+
     this.barChartData = [
       {
         data: [trainingMapped, reservedMapped, selectedMapped, confirmedMapped],
