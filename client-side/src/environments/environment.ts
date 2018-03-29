b26af736ceba7923a15791323dc6b9085f781597
Christian Diaz <cdiaz8369@hotmail.com>
2018-03-29 10:56:41 -0400

    Just making sure

--- client-side/src/environments/environment.ts
+++ client-side/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   context: context, // change for what the production environment would actually be
   url: 'http://54.166.255.85:8085/',
   msurl: 'http://34.227.178.103:',
-  assets: 'http://52.70.53.136:8086/angular/assets/',
+  assets: 'http://52.87.205.55:8086/angular/assets/',
   //assets: '../../../../../assets/', //url for local testing
 
   assessment: {
