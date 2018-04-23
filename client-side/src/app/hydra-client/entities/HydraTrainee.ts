6206a81cea0f960e913762b5bab336863e9183ac
shaleen <shaleen.anwar@gmail.com>
2018-04-23 11:30:33 -0400

    Constructor added to HydraTrainee

--- client-side/src/app/hydra-client/entities/HydraTrainee.ts
+++ client-side/src/app/hydra-client/entities/HydraTrainee.ts
@@ -22,4 +22,32 @@ export class HydraTrainee {
     client: string;
     endClient: string;
     traineeUserInfo: HydraUser;
+
+    constructor(traineeId: number, resourceId: number, trainingStatus: string, batch: HydraBatch, 
+        phoneNumber: string, skypeId: string, profileUrl: string,recruiterName: string, 
+        college: string, degree: string,major: string, techScreenerName: string,
+        projectCompletion: string, flagStatus: string, flagNotes: string, marketingStatus: string,
+        client: string, endClient: string, traineeUserInfo: HydraUser) {
+            this.traineeId = traineeId;
+            this.resourceId = resourceId;
+            this.trainingStatus = trainingStatus;
+            this.batch = batch;
+            this.phoneNumber = phoneNumber;
+            this.skypeId = skypeId;
+            this.profileUrl = profileUrl;
+            this.recruiterName = recruiterName;
+            this.college = college;
+            this.degree = degree;
+            this.major = major;
+            this.techScreenerName = techScreenerName;
+            this.projectCompletion = projectCompletion;
+            this.flagStatus = flagStatus;
+            this.flagNotes = flagNotes;
+            this.marketingStatus = marketingStatus;
+            this.client = client;
+            this.endClient = endClient;
+            this.traineeUserInfo = traineeUserInfo;
+      }
 }
+
+
