1c562b2127471b690054c6825971b1ffe5b7b773
shaleen <shaleen.anwar@gmail.com>
2018-04-23 14:24:52 -0400

    Updated HydraTrainee entity to Trainee and extended User, and updated the constructor.

--- client-side/src/app/hydra-client/entities/HydraTrainee.ts
+++ client-side/src/app/hydra-client/entities/HydraTrainee.ts
@@ -1,38 +1,41 @@
 import { HydraBatch } from './HydraBatch';
 import { User } from './User';
+import { UserRole } from './UserRole';
 
 
-export class HydraTrainee {
-    traineeId: number;
-    resourceId: number;
-    trainingStatus: string;
-    batch: HydraBatch;
-    phoneNumber: string;
-    skypeId: string;
-    profileUrl: string;
-    recruiterName: string;
-    college: string;
-    degree: string;
-    major: string;
-    techScreenerName: string;
-    projectCompletion: string;
-    flagStatus: string;
-    flagNotes: string;
-    marketingStatus: string;
-    client: string;
-    endClient: string;
-    traineeUserInfo: User;
+export class Trainee extends User {
+  
+    private resourceId: number;
+    private trainingStatus: string;
+    private batch: HydraBatch;
+    private skypeId: string;
+    private profileUrl: string;
+    private recruiterName: string;
+    private college: string;
+    private degree: string;
+    private major: string;
+    private techScreenerName: string;
+    private projectCompletion: string;
+    private flagStatus: string;
+    private flagNotes: string;
+    private marketingStatus: string;
+    private client: string;
+    private endClient: string;
+   
 
-    constructor(traineeId: number, resourceId: number, trainingStatus: string, batch: HydraBatch, 
+    constructor( userId: number, firstName: string, middleName: string, lastName: string,
+        email: string, password: string, backupPassword: string, role: UserRole,
+        mobilePhone: string, homePhone: string, token: string,resourceId: number, trainingStatus: string, batch: HydraBatch, 
         phoneNumber: string, skypeId: string, profileUrl: string,recruiterName: string, 
         college: string, degree: string,major: string, techScreenerName: string,
         projectCompletion: string, flagStatus: string, flagNotes: string, marketingStatus: string,
-        client: string, endClient: string, traineeUserInfo: HydraUser) {
-            this.traineeId = traineeId;
-            this.resourceId = resourceId;
+        client: string, endClient: string) {
+            super(userId, firstName, middleName, lastName, email, password,
+                backupPassword, role, mobilePhone, homePhone, token)
+                
+            this.resourceId = resourceId; 
             this.trainingStatus = trainingStatus;
             this.batch = batch;
-            this.phoneNumber = phoneNumber;
             this.skypeId = skypeId;
             this.profileUrl = profileUrl;
             this.recruiterName = recruiterName;
@@ -46,7 +49,7 @@ export class HydraTrainee {
             this.marketingStatus = marketingStatus;
             this.client = client;
             this.endClient = endClient;
-            this.traineeUserInfo = traineeUserInfo;
+
       }
 }
 
