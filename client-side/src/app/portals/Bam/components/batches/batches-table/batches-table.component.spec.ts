08c198d96874d1955471c0543c459837cd1ec065
Holden <tblkidhdo@yahoo.com>
2018-04-24 16:26:47 -0400

    Added unit test on BatchesTableComponent

--- client-side/src/app/portals/Bam/components/batches/batches-table/batches-table.component.spec.ts
+++ client-side/src/app/portals/Bam/components/batches/batches-table/batches-table.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BatchesTableComponent } from './batches-table.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { Dependencies } from '../../../bam.test.module';
+import { Batch } from '../../../models/batch.model';
 
 describe('BatchesTableComponent', () => {
   let component: BatchesTableComponent;
@@ -20,5 +21,17 @@ describe('BatchesTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   * Tests if functionality within ngOnInit() executes as expected.
+   */
+  it ('should set filtered to an array with one empty batch', () => {
+    const testBatch = new Batch(0, '', null, null, null, 0, 0);
+    component.batches = [testBatch];
+    component.ngOnInit();
+    expect(component.filtered).toEqual([testBatch]);
   });
 });
