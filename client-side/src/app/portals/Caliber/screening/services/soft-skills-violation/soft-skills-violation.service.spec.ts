e30f1e6d6d549e3fb40e27bcaea98d512515cb4f
musicrighter <musicrighter@gmail.com>
2018-04-25 14:00:15 -0400

    Hydra client and service.service fix

--- client-side/src/app/portals/Caliber/screening/services/soft-skills-violation/soft-skills-violation.service.spec.ts
+++ client-side/src/app/portals/Caliber/screening/services/soft-skills-violation/soft-skills-violation.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { SoftSkillsViolationServiceService } from './soft-skills-violation-service.service';
+import { SoftSkillsViolationService } from './soft-skills-violation.service';
 
 describe('SoftSkillsViolationServiceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SoftSkillsViolationServiceService]
+      providers: [SoftSkillsViolationService]
     });
   });
 
-  it('should be created', inject([SoftSkillsViolationServiceService], (service: SoftSkillsViolationServiceService) => {
+  it('should be created', inject([SoftSkillsViolationService], (service: SoftSkillsViolationService) => {
     expect(service).toBeTruthy();
   }));
 });
