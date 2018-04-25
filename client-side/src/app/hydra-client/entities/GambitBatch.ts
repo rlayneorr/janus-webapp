5f5f412b68867bdd6d15b0a05ad5f3d1f35da787
David Saeva <dsaeva@localhost.localdomain>
2018-04-25 17:00:31 -0400

    completed gambit batch entity implementation, should completely mirror gambit-batch-service batch class

--- client-side/src/app/hydra-client/entities/GambitBatch.ts
+++ client-side/src/app/hydra-client/entities/GambitBatch.ts
@@ -2,50 +2,44 @@ export class GambitBatch {
 
   batchId: number;
   resourceId: number;
-  goodGradeThreshold: number;
-
-  addressId: number;
-
-  borderlineGradeThreshold: number;
-  week: number;
   trainingName: string;
-
   trainerId: number;
   cotrainerId: number;
-
   skillTypeId: number;
-
+  addressId: number;
   location: string;
+  goodGradeThreshold: number;
+  borderlineGradeThreshold: number;
   startDate: Date;
   endDate: Date;
-
+  week: number;
   noteIds: number[];
-
   traineeIds: number[];
 
   /**
    * Initialize empty batch for BAM service
    *
    */
-  constructor(batchId: number = 0, resourceId: number = 0, goodGradeThreshold: number = 0,
-              addressId: number = 0, borderlineGradeThreshold: number = 0, week: number = 0,
-              trainingName: string = '', trainerId: number = 0, cotrainerId: number = 0,
-              skillTypeId: number = 0, location: string = '', startDate: Date = new Date(),
-              endDate: Date = new Date(), noteIds: number[] = [], traineeIds: number[] = [] ) {
+  constructor(batchId: number = 0, resourceId: number = 0, trainingName: string = '',
+              trainerId: number = 0, cotrainerId: number = 0, skillTypeId: number = 0,
+              addressId: number = 0, location: string = '', goodGradeThreshold: number = 0,
+              borderlineGradeThreshold: number = 0, startDate: Date = new Date(),
+              endDate: Date = new Date(), week: number = 0, noteIds: number[] = [],
+              traineeIds: number[] = [] ) {
 
       this.batchId = batchId;
       this.resourceId = resourceId;
-      this.goodGradeThreshold = goodGradeThreshold;
-      this.addressId = addressId;
-      this.borderlineGradeThreshold = borderlineGradeThreshold;
-      this.week = week;
       this.trainingName = trainingName;
       this.trainerId = trainerId;
       this.cotrainerId = cotrainerId;
       this.skillTypeId = skillTypeId;
+      this.addressId = addressId;
       this.location = location;
+      this.goodGradeThreshold = goodGradeThreshold;
+      this.borderlineGradeThreshold = borderlineGradeThreshold;
       this.startDate = startDate;
       this.endDate = endDate;
+      this.week = week;
       this.noteIds = noteIds;
       this.traineeIds = traineeIds;
 
