ae94e10dee72ff6f1ceda576445aa64fd32ae8e9
Luis Munoz <l.f.munoz@live.com>
2018-04-26 11:00:33 -0400

    commit at 11:00 AM

--- client-side/src/app/services/location/location.service.ts
+++ client-side/src/app/services/location/location.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Location } from '../../entities/Location';
+
 
 @Injectable()
 export class LocationService {
+  location: Location;
+  url: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
+  getLocation(request: any) {
+    return this.httpClient.get<Location>(this.url);
+  }
+  newLocation(location: Location) {
+    return this.httpClient.post<Location>(this.url, location);
+  }
+  updateLocation(location: Location) {
+    return this.httpClient.post<Location>(this.url, location);
+  }
+  deleteLocaiton(location: Location) {
+    return this.httpClient.post<Location>(this.url, location);
+  }
 }
