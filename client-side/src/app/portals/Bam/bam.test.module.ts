f2b8daffcd8e0572febbef3126cc4dd2e38fe8f8
Holden <tblkidhdo@yahoo.com>
2018-04-26 14:54:04 -0400

    Added UrlService to bam.test.module.ts

--- client-side/src/app/portals/Bam/bam.test.module.ts
+++ client-side/src/app/portals/Bam/bam.test.module.ts
@@ -69,6 +69,7 @@ import { AlertsComponent } from './components/alerts/alerts.component';
 import { AlertService } from './services/alert.service';
 import { ExistingSubtopicModalComponent } from './components/calendar/existing-subtopic-modal/existing-subtopic-modal.component';
 
+import { UrlService } from '../../hydra-client/services/urls/url.service';
 
 export const Dependencies = {
   imports: [
@@ -150,6 +151,7 @@ export const Dependencies = {
     ChildrenOutletContexts,
     AssignforcesyncService,
     AlertService,
+    UrlService,
   ],
   exports: [
     SearchPipe,
