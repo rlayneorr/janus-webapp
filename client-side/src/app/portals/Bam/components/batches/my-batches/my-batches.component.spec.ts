fd754af41657cb83ca257d49f6b0ad5beb23a1e1
Holden <tblkidhdo@yahoo.com>
2018-04-26 14:36:54 -0400

    Added more tests for MyBatchesComponent. Fixed null and undefined interaction for getBatches. All tests pass.

--- client-side/src/app/portals/Bam/components/batches/my-batches/my-batches.component.spec.ts
+++ client-side/src/app/portals/Bam/components/batches/my-batches/my-batches.component.spec.ts
@@ -1,22 +1,68 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, Component } from '@angular/core';
 
 import { MyBatchesComponent } from './my-batches.component';
 import { Dependencies } from '../../../bam.test.module';
 import { filter } from 'rxjs/operator/filter';
 import { Batch } from '../../../models/batch.model';
+import { BatchService } from '../../../services/batch.service';
+import { SessionService } from '../../../services/session.service';
+import { BamUser } from '../../../models/bamuser.model';
+import { UsersService } from '../../../services/users.service';
+import { CurriculumService } from '../../../services/curriculum.service';
+
+import { Observable } from 'rxjs/Observable';
+import { Curriculum } from '../../../models/curriculum.model';
 
 fdescribe('MyBatchesComponent', () => {
   let component: MyBatchesComponent;
-  let fixture; // : ComponentFixture<MyBatchesComponent>;
+  let fixture: ComponentFixture<MyBatchesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule(Dependencies).compileComponents();
   }), 1440000);
 
   beforeEach(() => {
+
+    // This creates instances of the services you intend to mock, with dependencies inected according to your TestBed's testing module.
+    const sessionService: SessionService = TestBed.get(SessionService);
+    const usersService: UsersService = TestBed.get(UsersService);
+    const curriculumService: CurriculumService = TestBed.get(CurriculumService);
+    const batchService: BatchService = TestBed.get(BatchService);
+
+    const tempUser = new BamUser(2, 'TestF', 'TestM', 'TestL',
+      'test@fake.email', 'password', 1, null, '123-456-7890',
+      '098-765-4321', 'TestSkype', 'notPassword', 234);
+
+    const tempBatch = new Batch(0, '', null, null, tempUser, 7, 29);
+    tempBatch.trainerID = 7;
+
+
+    // This is where you overwrite the base functionality of your services using a Spy.and chain.
+    // SpyOn(Service, 'Function').and.callFake(Function to use instead);
+    spyOn(sessionService, 'getUser').and.returnValue(tempUser);
+
+    spyOn(usersService, 'getUserByID').and.callFake( (userId: number) => {
+      if (userId === 7) {
+        return Observable.of(tempUser);
+      } else {
+        return Observable.throw(tempUser);
+      }
+    }); // .returnValues(Observable.of(tempUser), Observable.throw('This is an Error'),
+     // Observable.throw('This is an Error'), Observable.throw('This is an Error'), Observable.throw('This is an Error'));
+
+     // Or, if you only need it to return a spcific value
+     // SpyOn(Service, 'Function').and.returnValue(Value that function should return when called);
+    spyOn(curriculumService, 'getCurriculumById').and.returnValue(Observable.of(new Curriculum()));
+
+    spyOn(batchService, 'getAllBatchesInProgress').and.returnValue(Observable.of([tempBatch]));
+    spyOn(batchService, 'getPastBatches').and.returnValue(Observable.of([tempBatch]));
+    spyOn(batchService, 'getFutureBatches').and.returnValue(Observable.of([tempBatch]));
+
+    // Then you can proceed as normal, and your mocked functionality will be used automatically.
     fixture = TestBed.createComponent(MyBatchesComponent);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -78,7 +124,7 @@ fdescribe('MyBatchesComponent', () => {
    * @batch 1803 usf
    * Tests if setFilterText is called when the change event is fired.
    */
-  it ('should run setFilterText when a change is made to the filter text', async(() => {
+  it('should run setFilterText when a change is made to the filter text', async(() => {
 
     const filterElement = fixture.debugElement.query(By.css('.pull-right'));
 
@@ -96,7 +142,7 @@ fdescribe('MyBatchesComponent', () => {
    * @batch 1803 usf
    * Tests if filterText is set to value of event target
    */
-  it ('should set filterText to "This is a Unit Test"', async(() => {
+  it('should set filterText to "This is a Unit Test"', async(() => {
 
     const filterElement = fixture.debugElement.query(By.css('.pull-right')).nativeElement;
 
@@ -114,8 +160,9 @@ fdescribe('MyBatchesComponent', () => {
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Tests if setBatches properly handles invalid input of type undefined
    */
-  it ('should set batches to an empty array if input is undefined.', () => {
+  it('should set batches to an empty array if input is undefined.', () => {
     component.setbatches(undefined);
 
     expect(component.batches).toEqual([]);
@@ -124,8 +171,9 @@ fdescribe('MyBatchesComponent', () => {
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Tests if setBatches properly handles invalid input of type null
    */
-  it ('should set batches to an empty array if input is null.', () => {
+  it('should set batches to an empty array if input is null.', () => {
     component.setbatches(null);
 
     expect(component.batches).toEqual([]);
@@ -134,18 +182,95 @@ fdescribe('MyBatchesComponent', () => {
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Tests that setBatches handles valid input as expected
    */
-  it ('should set batches equal to an input batch array.', () => {
-     const batches: Array<Batch> = [
-      new Batch(0, null, null, null, null, 0, 0),
-      new Batch(0, null, null, null, null, 0, 0),
-      new Batch(0, null, null, null, null, 0, 0),
-      new Batch(0, null, null, null, null, 0, 0),
-      new Batch(0, null, null, null, null, 0, 0),
-     ];
+  it('should set batches equal to an input batch array.', () => {
+    const batches: Array<Batch> = [
+      new Batch(0, null, null, null, null, 7, 29),
+      new Batch(5, null, null, null, null, 7, 31),
+      new Batch(1, null, null, null, null, 7, 37),
+      new Batch(3, null, null, null, null, 7, 41),
+      new Batch(2, null, null, null, null, 7, 47),
+    ];
+    component.setbatches(batches);
 
-     component.setbatches(batches);
-
-     expect(component.batches).toEqual(batches);
+    expect(component.batches).toEqual(batches);
   });
+
+  /**
+  * @author Holden Olivier
+  * @batch 1803 usf
+  * Tests that a batch's trainer is set to null on error
+  */
+  it ('should set batch trainer to null if usersService returns an error', () => {
+    const tempUser: BamUser = new BamUser(0, '', '', '', '', '', 0, null, '' , '', '', '', 0);
+    const batches: Array<Batch> = [
+    new Batch(0, '', null, null, tempUser, 11, 29),
+    new Batch(5, '', null, null, tempUser, 11, 31),
+    new Batch(1, '', null, null, tempUser, 11, 37),
+    new Batch(3, '', null, null, tempUser, 11, 41),
+    new Batch(2, '', null, null, tempUser, 11, 47),
+    ];
+
+    component.setbatches(batches);
+
+    expect(component.batches[1].trainer).toBeNull();
+  });
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   * Tests if loadCurrent sets batches to the array returned by the batchService
+   */
+  it('should set batches to an array with one batch in it', async( () => {
+    component.loadCurrent();
+
+    const tempUser = new BamUser(2, 'TestF', 'TestM', 'TestL',
+    'test@fake.email', 'password', 1, null, '123-456-7890',
+    '098-765-4321', 'TestSkype', 'notPassword', 234);
+
+    const expected = [new Batch(0, '', null, null, tempUser, 7, 29)];
+    expected[0].curriculum = undefined;
+    expected[0].trainerID = 7;
+
+    expect(component.batches).toEqual(expected);
+  }));
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   * Tests if loadPast sets batches to the array returned by the batchService
+   */
+  it('should set batches to an array with one batch in it', async( () => {
+    component.loadPast();
+
+    const tempUser = new BamUser(2, 'TestF', 'TestM', 'TestL',
+    'test@fake.email', 'password', 1, null, '123-456-7890',
+    '098-765-4321', 'TestSkype', 'notPassword', 234);
+
+    const expected = [new Batch(0, '', null, null, tempUser, 7, 29)];
+    expected[0].curriculum = undefined;
+    expected[0].trainerID = 7;
+
+    expect(component.batches).toEqual(expected);
+  }));
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   * Tests if loadFuture sets batches to the array returned by the batchService
+   */
+  it('should set batches to an array with one batch in it', async( () => {
+    component.loadFuture();
+
+    const tempUser = new BamUser(2, 'TestF', 'TestM', 'TestL',
+    'test@fake.email', 'password', 1, null, '123-456-7890',
+    '098-765-4321', 'TestSkype', 'notPassword', 234);
+
+    const expected = [new Batch(0, '', null, null, tempUser, 7, 29)];
+    expected[0].curriculum = undefined;
+    expected[0].trainerID = 7;
+
+    expect(component.batches).toEqual(expected);
+  }));
 });
