033eed79d64d167024cda15c507b5c037d7a638f
Luis Munoz <l.f.munoz@live.com>
2018-04-26 14:11:28 -0400

    forgot something

--- client-side/src/app/hydra-client/entities/location-entities/Building.ts
+++ client-side/src/app/hydra-client/entities/location-entities/Building.ts
@@ -2,12 +2,12 @@ export class Building {
     buildingId: number;
     streetAddress: string;
     locationId: number;
-    buildingNum: number;
-    constructor( buildingId: number, streetAddress: string, locationId: number, buildingNum: number ) {
+    buildingNumber: number;
+    constructor( buildingId: number, streetAddress: string, locationId: number, buildingNumber: number ) {
         this.buildingId = buildingId;
         this.streetAddress = streetAddress;
         this.locationId = locationId;
-        this.buildingNum = buildingNum;
+        this.buildingNumber = buildingNumber;
     }
 
 }
