14e3d2f31d3e18647b8aa2b50b677175d40e084d
Lex Gospodinoff <lex.gospodinoff@gmail.com>
2018-04-26 13:50:17 -0400

    restored test.ts

--- client-side/src/test.ts
+++ client-side/src/test.ts
@@ -25,7 +25,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-// TEMPORARILY restrict to batch-details.component
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
