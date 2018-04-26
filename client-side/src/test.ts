c20d44364826e48196b0f7213e00dd10cbacea75
Lex Gospodinoff <lex.gospodinoff@gmail.com>
2018-04-26 11:44:00 -0400

    restored test.ts

--- client-side/src/test.ts
+++ client-side/src/test.ts
@@ -26,7 +26,7 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 // TEMPORARILY restrict to batch-details.component
-const context = require.context('./', true, /batch-details\.component\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
