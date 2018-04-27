e4a906e2436edbba8c44d4b6124dd16a7017c51b
dzuniga2 <dzuniga546@gmail.com>
2018-04-27 09:35:16 -0400

    testing changes ti welcome.component.ts

--- client-side/src/app/portals/Bam/bam.test-observable.module.ts
+++ client-side/src/app/portals/Bam/bam.test-observable.module.ts
@@ -68,6 +68,9 @@ import { Ng2OrderModule } from 'ng2-order-pipe';
 import { AlertsComponent } from './components/alerts/alerts.component';
 import { AlertService } from './services/alert.service';
 import { ExistingSubtopicModalComponent } from './components/calendar/existing-subtopic-modal/existing-subtopic-modal.component';
+import { UrlService } from '../../hydra-client/services/urls/url.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 
 export const Dependencies = {
   imports: [
@@ -147,6 +150,10 @@ export const Dependencies = {
     ChildrenOutletContexts,
     AssignforcesyncService,
     AlertService,
+    UrlService,
+    WelcomeComponent,
+    HttpClientTestingModule,
+    HttpTestingController
   ],
   exports: [
     SearchPipe,
