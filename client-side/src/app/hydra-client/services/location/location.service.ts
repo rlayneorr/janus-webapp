7e41df1c4ba5afd89c8bf6f40de4bdc8c3b800f5
Tanhim Ahmed <tia8@pitt.edu>
2018-04-28 18:38:11 -0400

    updating location services to work in Assign-Force

--- client-side/src/app/hydra-client/services/location/location.service.ts
+++ client-side/src/app/hydra-client/services/location/location.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Location } from '../../entities/location-entities/Location';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { UrlService } from '../urls/url.service';
 import { Building } from '../../entities/location-entities/Building';
 import { Room } from '../../entities/location-entities/Room';
+import { Unavailability } from '../../entities/location-entities/Unavailability';
 
 
 @Injectable()
@@ -17,7 +18,7 @@ export class LocationService {
 
   // get all Locations //
   getAllLocations() {
-    return this.httpClient.get<Location>(this.urls.location.getAllLocations()).subscribe(
+    return this.httpClient.get<Location[]>(this.urls.location.getAllLocations()).subscribe(
       (payload) => {
         this.location.next(payload);
         console.log(payload);
@@ -25,57 +26,81 @@ export class LocationService {
     );
   }
   // get Location by Id //
-  getLocation(location: Location) {
-    return this.httpClient.get<Location>(this.urls.location.getLocationById(location.locationId));
+  getLocation(location: any) {
+    return this.httpClient.get<Location>(this.urls.location.getLocationById(location)).subscribe(
+      (payload) => {
+        this.location.next(payload);
+        console.log(payload);
+      }
+    );
   }
   // set new Location //
-  newLocation(location: Location) {
-    return this.httpClient.post<Location>(this.urls.location.postLocation(), JSON.stringify(location));
+  newLocation(location: any) {
+    let header = new HttpHeaders();
+    header = header.set('Content-Type', 'application/json; charset=utf-8;' );
+    return this.httpClient.post<Location>(this.urls.location.postLocation(), JSON.stringify(location), {headers: header}).subscribe(
+      (payload) => {
+        console.log(payload);
+      }
+    );
   }
   // update the location //
-  updateLocation(location: Location) {
+  updateLocation(location: any) {
     return this.httpClient.post<Location>(this.urls.location.putLocationById(location.locationId), JSON.stringify(location));
   }
   // set location as inactive //
-  deleteLocation(location: Location) {
+  deleteLocation(location: any) {
     return this.httpClient.post<Location>(this.urls.location.deleteLocationById(location.locationId), JSON.stringify(location));
   }
+
   // get all Buildings //
   getAllBuildings() {
-    return this.httpClient.get<Building>(this.urls.building.getAllBuildings());
+    return this.httpClient.get<Building[]>(this.urls.building.getAllBuildings());
   }
   // get Building by Id //
-  getOneBuilding(building: Building) {
+  getOneBuilding(building: any) {
     return this.httpClient.get<Building>(this.urls.building.getBuildingById(building.buildingId));
   }
   // set new Building //
-  newBuilding(building: Building) {
+  newBuilding(building: any) {
     return this.httpClient.post<Building>(this.urls.building.postBuilding(), JSON.stringify(building));
   }
   // update Building //
-  updateBuilding(building: Building) {
+  updateBuilding(building: any) {
     return this.httpClient.put<Building>(this.urls.building.putBuildingById(building.buildingId), JSON.stringify(building));
   }
   // set Building as inactive //
-  deleteBuilding(building: Building) {
+  deleteBuilding(building: any) {
     return this.httpClient.delete<Building>(this.urls.building.deleteBuildingById(building.buildingId));
   }
 
+  // get all Rooms //
   getAllRooms() {
-    return this.httpClient.get<Room>(this.urls.room.getAllRooms());
+    return this.httpClient.get<Room[]>(this.urls.room.getAllRooms());
   }
-  getOneRoom(room: Room) {
+  // get Room by Id //
+  getOneRoom(room: any) {
     return this.httpClient.get<Room>(this.urls.room.getRoomById(room.roomId));
   }
-  newRoom(room: Room) {
+  // set new Room //
+  newRoom(room: any) {
     return this.httpClient.post<Room>(this.urls.room.postRoom(), JSON.stringify(room));
   }
-  updateRoom(room: Room) {
+  // update Room //
+  updateRoom(room: any) {
     return this.httpClient.put<Room>(this.urls.room.putRoomById(room.roomId), JSON.stringify(room));
   }
-  deleteRoom(room: Room) {
+  // set Room as inactive //
+  deleteRoom(room: any) {
     return this.httpClient.delete<Room>(this.urls.room.deleteRoomById(room.roomId));
   }
 
-
+  // get all Unavailabilities //
+  getAllUnavailabilities() {
+    return this.httpClient.get<Unavailability[]>(this.urls.unavailability.getAllUnavailabilities());
+  }
+  // get Unavailability by Id //
+  getOneUnavailability(unavailability: any) {
+    return this.httpClient.post<Unavailability>(this.urls.unavailability.postUnavailability(), JSON.stringify(unavailability));
+  }
 }
--- client-side/src/app/portals/Assign-Force/home/home.component.ts
+++ client-side/src/app/portals/Assign-Force/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { ChuckNorrisService } from '../../../services/chuck-norris.service';
+import { LocationService } from '../../../hydra-client/services/location/location.service';
+import { Location } from '../../../hydra-client/entities/location-entities/Location';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +13,18 @@ export class HomeComponent implements OnInit, OnDestroy {
   private jokeSubscription: Subscription;
   joke;
 
-  constructor(private chuckNorrisService: ChuckNorrisService) { }
+  constructor(private chuckNorrisService: ChuckNorrisService, private locationService: LocationService) { }
 
   ngOnInit() {
     this.jokeSubscription = this.chuckNorrisService.joke$.subscribe( (resp) => {
       this.joke = resp;
     });
+    this.locationService.getAllLocations(); // This one currently works. get all locations
+    this.locationService.getLocation(1); // this one currently works. get a specific location.
+
+    // tslint:disable-next-line:max-line-length
+    this.locationService.newLocation(new Location(null, 'eatmyshorts', 'Los Angeles', 'CA', 3252, 'usdfa', true)); // this also works. create new location.
+
   }
 
   newJoke() {
