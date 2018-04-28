0e68304a877937368135ee74d2e246f3c8ea8c6c
Lex Gospodinoff <lex.gospodinoff@gmail.com>
2018-04-28 16:50:35 -0400

    testing location service

--- client-side/src/app/hydra-client/services/location/location.service.spec.ts
+++ client-side/src/app/hydra-client/services/location/location.service.spec.ts
@@ -1,15 +1,37 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { LocationService } from './location.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { UrlService } from '../urls/url.service';
 
 describe('LocationService', () => {
+
+  beforeAll(() => {
+
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LocationService]
+      providers: [
+        LocationService,
+        UrlService],
+      imports: [HttpClientModule]
     });
   });
 
   it('should be created', inject([LocationService], (service: LocationService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should get all locations', inject([LocationService], (service: LocationService) => {
+    const expectedLocation = JSON.parse(`{"locationId": 1, "street": "102 bluh ave", "city": "Lutz",
+    "state": "fl", "zip": null, "company": null, "active": true }`);
+    let data;
+    service.getAllLocations().subscribe((result) => {
+      console.log(result);
+      data = result;
+    });
+    expect(data).toBe(expectedLocation);
+  }));
+
 });
