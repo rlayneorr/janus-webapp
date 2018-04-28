12afea07a3bcb890b549ba02070548a47ca987cc
Lex Gospodinoff <lex.gospodinoff@gmail.com>
2018-04-28 17:46:37 -0400

    started on testing

--- client-side/src/app/hydra-client/services/location/location.service.spec.ts
+++ client-side/src/app/hydra-client/services/location/location.service.spec.ts
@@ -3,6 +3,12 @@ import { TestBed, inject } from '@angular/core/testing';
 import { LocationService } from './location.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { UrlService } from '../urls/url.service';
+import { Location } from '../../entities/location-entities/Location';
+
+const exp = {
+  location: JSON.parse(`{"locationId": 1, "street": "102 bluh ave", "city": "Lutz",
+  "state": "fl", "zip": null, "company": null, "active": true }`)
+};
 
 describe('LocationService', () => {
 
@@ -23,15 +29,12 @@ describe('LocationService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should get all locations', inject([LocationService], (service: LocationService) => {
-    const expectedLocation = JSON.parse(`{"locationId": 1, "street": "102 bluh ave", "city": "Lutz",
-    "state": "fl", "zip": null, "company": null, "active": true }`);
-    let data;
+  it('should get data from the backend', inject([LocationService], (service: LocationService) => {
+    let data: Location;
     service.getAllLocations().subscribe((result) => {
-      console.log(result);
       data = result;
     });
-    expect(data).toBe(expectedLocation);
+    expect(data).toBe([exp.location]);
   }));
 
 });
