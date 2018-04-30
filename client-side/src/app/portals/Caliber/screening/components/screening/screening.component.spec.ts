7d28d5c7c04287670fbff41e18237aaaba9f73cd
musicrighter <musicrighter@gmail.com>
2018-04-30 16:29:41 -0400

    screening test

--- client-side/src/app/portals/Caliber/screening/components/screening/screening.component.spec.ts
+++ client-side/src/app/portals/Caliber/screening/components/screening/screening.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScreeningComponent } from './screening.component';
+import { RouterOutlet, ChildrenOutletContexts } from '@angular/router';
 
-describe('ScreeningComponent', () => {
+fdescribe('ScreeningComponent', () => {
   let component: ScreeningComponent;
   let fixture: ComponentFixture<ScreeningComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScreeningComponent ]
+      declarations: [ ScreeningComponent, RouterOutlet ],
+      providers: [ ChildrenOutletContexts ]
     })
     .compileComponents();
   }));
