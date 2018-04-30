538608aee9a045001bfe182bc82a0e11cb50a6d5
musicrighter <musicrighter@gmail.com>
2018-04-30 16:55:57 -0400

    revomed downgrade added final-report

--- client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.spec.ts
+++ client-side/src/app/portals/Caliber/screening/components/final-report/final-report.component.spec.ts
@@ -1,14 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FinalReportComponent } from './final-report.component';
+import { ScreeningService } from '../../services/screening/screening.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { UrlUtilService } from '../../services/UrlUtil/url-util.service';
+import { SimpleTraineeService } from '../../services/simpleTrainee/simple-trainee.service';
+import { SkillTypeService } from '../../services/skillType/skill-type.service';
+import { SkillTypeBucketService } from '../../services/skillTypeBucketLookup/skill-type-bucket.service';
+import { QuestionScoreService } from '../../services/question-score/question-score.service';
+import { ScoresToBucketsUtil } from '../../util/scoresToBuckets.util';
+import { AlertService } from '../../../../Bam/services/alert.service';
+import { AlertsService } from '../../../services/alerts.service';
+import { SoftSkillsViolationService } from '../../services/soft-skills-violation/soft-skills-violation.service';
 
-describe('FinalReportComponent', () => {
+fdescribe('FinalReportComponent', () => {
   let component: FinalReportComponent;
   let fixture: ComponentFixture<FinalReportComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FinalReportComponent ]
+      declarations: [ FinalReportComponent ],
+      providers: [ ScreeningService, HttpClient, HttpHandler, UrlUtilService, SimpleTraineeService, SkillTypeService,
+        SkillTypeBucketService, QuestionScoreService, ScoresToBucketsUtil, AlertsService, SoftSkillsViolationService ]
     })
     .compileComponents();
   }));
@@ -19,7 +32,13 @@ describe('FinalReportComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  it('should be check', () => {
+    component.copyToClipboard();
+    expect(component.checked).toBeTruthy();
+  })
+
 });
