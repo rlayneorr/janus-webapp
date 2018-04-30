279b565f1b33e51cbece3a77b4097650c5440b43
Holden <tblkidhdo@yahoo.com>
2018-04-30 16:57:34 -0400

    Added unit test for TopicPoolComponent.getSubTopics

--- client-side/src/app/portals/Bam/components/curriculum-editor/topic-pool/topic-pool.component.spec.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/topic-pool/topic-pool.component.spec.ts
@@ -4,16 +4,38 @@ import { TopicPoolComponent } from './topic-pool.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { Dependencies } from '../../../bam.test.module';
 import { Topic } from '../../../models/topic.model';
+import { SubtopicService } from '../../../services/subtopic.service';
+import { Observable } from 'rxjs/Observable';
+import { SubtopicCurric } from '../../../models/subtopicCurric.model';
 
 fdescribe('TopicPoolComponent', () => {
   let component: TopicPoolComponent;
   let fixture: ComponentFixture<TopicPoolComponent>;
+
+  let parentTopic: Topic;
+  let notParentTopic: Topic;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule(Dependencies).compileComponents();
   }), 1440000);
 
   beforeEach(() => {
+
+    parentTopic = {topicID: 256, topicName: 'Parent Topic'};
+    notParentTopic = {topicID: 512, topicName: 'notParentTopic'};
+
+    const subtopicService: SubtopicService = TestBed.get(SubtopicService);
+
+    spyOn(subtopicService, 'getAllSubtopics').and.returnValue(Observable.of<SubtopicCurric[]>([
+      {date: {day: 0, endTime: 1, startTime: 0, week: 6}, parentTopic: parentTopic, status: 'inProgress',
+      subtopicId: 0, subtopicName: 'Subtopic 1'},
+      {date: {day: 1, endTime: 1, startTime: 0, week: 6}, parentTopic: parentTopic, status: 'complete',
+      subtopicId: 0, subtopicName: 'Subtopic 2'},
+      {date: {day: 2, endTime: 1, startTime: 0, week: 6}, parentTopic: parentTopic, status: 'upcoming',
+      subtopicId: 0, subtopicName: 'Subtopic 3'},
+    ]
+    ));
+
     fixture = TestBed.createComponent(TopicPoolComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -93,5 +115,36 @@ fdescribe('TopicPoolComponent', () => {
     component.uniqueTopics();
 
     expect(component.uniqarrFiltered).toEqual(filteredTopics);
+  });
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   */
+  it ('should fill subTopicArray with arrays representing the subtopics of all topics represented within subTopicName', () => {
+    const Topics: Array<Topic> = [notParentTopic, { topicID: 0, topicName: 'FirstTopic' },
+    { topicID: 1, topicName: 'SecondTopic' }, parentTopic, { topicID: 2, topicName: 'ThirdTopic' }];
+
+    component.subTopicName = Topics;
+
+    component.getSubTopics();
+
+    const expected: SubtopicCurric[][] = [
+      [],
+      [],
+      [],
+      [
+        {date: {day: 0, endTime: 1, startTime: 0, week: 6}, parentTopic: parentTopic, status: 'inProgress',
+        subtopicId: 0, subtopicName: 'Subtopic 1'},
+        {date: {day: 1, endTime: 1, startTime: 0, week: 6}, parentTopic: parentTopic, status: 'complete',
+        subtopicId: 0, subtopicName: 'Subtopic 2'},
+        {date: {day: 2, endTime: 1, startTime: 0, week: 6}, parentTopic: parentTopic, status: 'upcoming',
+        subtopicId: 0, subtopicName: 'Subtopic 3'}
+      ],
+      []
+    ];
+
+    expect(component.subTopicArray.length === 1);
+    expect(component.subTopicArray).toEqual(expected);
   });
 });
