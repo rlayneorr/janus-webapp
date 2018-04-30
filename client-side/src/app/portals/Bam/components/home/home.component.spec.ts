45d4dd91f545094713155d2106cf4a77fbb4fa2c
Craig Koepele <craig.koepele@gmail.com>
2018-04-30 09:32:44 -0400

    Home test completed, no errors, not passing

--- client-side/src/app/portals/Bam/components/home/home.component.spec.ts
+++ client-side/src/app/portals/Bam/components/home/home.component.spec.ts
@@ -1,18 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { Dependencies } from '../../bam.test-observable.module';
+import { BamUser } from '../../models/bamuser.model';
+import { SessionService } from '../../services/session.service';
+import { of } from 'rxjs/observable/of';
+import { HomeUtil } from './home-test.util';
 
-describe('HomeComponent', () => {
+
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  const user: BamUser = HomeUtil.getUserById(2);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule(Dependencies).compileComponents();
   }), 1440000);
 
   beforeEach(() => {
+    const sessionService: SessionService = TestBed.get(SessionService);
+
+    spyOn(sessionService, 'putUserInSession').and.returnValue(of(user));
+
+    TestBed.overrideProvider(SessionService, { useValue: sessionService });
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,4 +33,9 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('home should get get a BamUser', async(() => {
+    component.ngOnInit();
+    expect(component.currentUser).toEqual(user);
+  }));
 });
