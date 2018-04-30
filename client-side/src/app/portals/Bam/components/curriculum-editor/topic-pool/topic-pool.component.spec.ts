6143fa199b5246ac3d694765d08656b3bda6be26
Holden <tblkidhdo@yahoo.com>
2018-04-30 11:48:30 -0400

    Added unit testing for TopicPoolComponent.onlyUnique

--- client-side/src/app/portals/Bam/components/curriculum-editor/topic-pool/topic-pool.component.spec.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/topic-pool/topic-pool.component.spec.ts
@@ -37,4 +37,39 @@ fdescribe('TopicPoolComponent', () => {
     expect(component.topics).toEqual(['FirstTopic', 'SecondTopic', 'ThirdTopic']);
   });
 
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   */
+  it ('should return true if the specified Topic appears only once in the array', () => {
+    const tempTopic: Topic = new Topic();
+
+    tempTopic.topicName = 'ThirdTopic';
+    tempTopic.topicID = 2;
+
+    const Topics: Array<Topic> = [{topicID: 0, topicName: 'FirstTopic'},
+      {topicID: 1, topicName: 'SecondTopic'}, tempTopic, {topicID: 3, topicName: 'FourthTopic'}];
+
+      const boolean = component.onlyUnique(tempTopic, 2, Topics);
+
+      expect(boolean).toBeTruthy();
+  });
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   */
+  it ('should return false if the specified index is not the first time the specified Topic appears in the array', () => {
+    const tempTopic: Topic = new Topic();
+
+    tempTopic.topicName = 'ThirdTopic';
+    tempTopic.topicID = 2;
+
+    const Topics: Array<Topic> = [{topicID: 0, topicName: 'FirstTopic'},
+      {topicID: 1, topicName: 'SecondTopic'}, tempTopic, {topicID: 3, topicName: 'FourthTopic'}, tempTopic];
+
+      const boolean = component.onlyUnique(tempTopic, 4, Topics);
+
+      expect(boolean).toBeFalsy();
+  });
 });
