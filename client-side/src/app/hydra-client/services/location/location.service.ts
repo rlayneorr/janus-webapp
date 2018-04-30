f281a1babf762c13ec840a3040c00b8a9ecbc468
Tanhim Ahmed <tia8@pitt.edu>
2018-04-30 09:43:35 -0400

    pushing updated location services, still need to add service dependencies. saving changes before pulling another branch

--- client-side/src/app/hydra-client/services/location/location.service.ts
+++ client-side/src/app/hydra-client/services/location/location.service.ts
@@ -69,51 +69,121 @@ export class LocationService {
 
   // get all Buildings //
   getAllBuildings() {
-    return this.httpClient.get<Building[]>(this.urls.building.getAllBuildings());
+    return this.httpClient.get<Array<Building>>(this.urls.building.getAllBuildings()).subscribe((payload) => {
+      console.log(payload);
+      this.buildings.next(payload);
+    });
   }
-  // get Building by Id //
-  getOneBuilding(building: any) {
-    return this.httpClient.get<Building>(this.urls.building.getBuildingById(building.buildingId));
+  // Get all buildings by Location ID. this one is dependent on a location's Id //
+  getBuildingsByLocationId(locationId: any) {
+    return this.httpClient.get<Array<Building>>(this.urls.building.getBuildingsByLocationId(locationId)).subscribe((payload) => {
+      console.log(payload);
+      this.buildings.next(payload);
+    });
+  }
+  // Returns a singular location by its ID. This has no corelation with locations.
+  getBuildingById(buildingId: any) {
+    return this.httpClient.get<Building>(this.urls.building.getBuildingById(buildingId)).subscribe((payload) => {
+      console.log(payload);
+      this.building.next(payload);
+    });
   }
   // set new Building //
-  newBuilding(building: any) {
-    return this.httpClient.post<Building>(this.urls.building.postBuilding(), JSON.stringify(building));
+  newBuilding(building: Building) {
+    let header = new HttpHeaders();
+    header = header.set('Content-Type', 'application/json; charset=utf-8;' );
+    return this.httpClient.post<Building>(this.urls.building.postBuilding(), JSON.stringify(building), {headers: header})
+    .subscribe((payload) => {
+    console.log(payload);
+    this.building.next(payload);
+    });
   }
   // update Building //
-  updateBuilding(building: any) {
-    return this.httpClient.put<Building>(this.urls.building.putBuildingById(building.buildingId), JSON.stringify(building));
+  updateBuilding(building: Building) {
+    let header = new HttpHeaders();
+    header = header.set('Content-Type', 'application/json; charset=utf-8;' );
+    return this.httpClient.put<Building>(this.urls.building.putBuildingById(building.buildingId),
+    JSON.stringify(building), {headers: header}).subscribe(
+      (payload) => {
+        this.buildings.next(payload);
+      }
+    );
   }
   // set Building as inactive //
-  deleteBuilding(building: any) {
-    return this.httpClient.delete<Building>(this.urls.building.deleteBuildingById(building.buildingId));
+  deleteBuilding(building: Building) {
+    return this.httpClient.delete<Building>(this.urls.building.deleteBuildingById(building.buildingId)).subscribe(
+      (payload) => {
+        this.building.next(payload);
+      }
+    );
   }
 
   // get all Rooms //
   getAllRooms() {
-    return this.httpClient.get<Room[]>(this.urls.room.getAllRooms());
+    return this.httpClient.get<Array<Room>>(this.urls.room.getAllRooms()).subscribe(
+      (payload) => {
+        this.rooms.next(payload);
+        console.log(payload);
+      }
+    );
   }
   // get Room by Id //
   getOneRoom(room: any) {
-    return this.httpClient.get<Room>(this.urls.room.getRoomById(room.roomId));
+    return this.httpClient.get<Room>(this.urls.room.getRoomById(room)).subscribe(
+      (payload) => {
+        this.room.next(payload);
+        console.log(payload);
+      }
+    );
   }
+  // getRoomsByLocationId(locationId: any) {
+  //   return this.httpClient.get<Array<Room>>(this.urls.room.getRoomsByLocationId(locationId))
+  //   .subscribe((payload) => {
+  //     this.rooms.next(payload);
+  //     console.log(payload);
+  //   });
+  // }
+
+  // get all Rooms in a Building //
+  getRoomsByBuildingId(buildingId: any) {
+    return this.httpClient.get<Array<Room>>(this.urls.room.getRoomsByBuildingId(buildingId))
+    .subscribe((payload) => {
+      this.rooms.next(payload);
+      console.log(payload);
+    });
+}
   // set new Room //
   newRoom(room: any) {
-    return this.httpClient.post<Room>(this.urls.room.postRoom(), JSON.stringify(room));
+    return this.httpClient.post<Room>(this.urls.room.postRoom(), JSON.stringify(room), {headers: this.header}).subscribe(
+      (payload) => {
+        this.room.next(payload);
+        console.log(payload);
+      }
+    );
   }
-  // update Room //
-  updateRoom(room: any) {
-    return this.httpClient.put<Room>(this.urls.room.putRoomById(room.roomId), JSON.stringify(room));
+  // update Room: TO-DO //
+  updateRoom(room: Room) {
+    return this.httpClient.put<Room>(this.urls.room.putRoomById(room.roomId), JSON.stringify(room), {headers: this.header}).subscribe(
+      (payload) => {
+        this.room.next(payload);
+        console.log(payload);
+      }
+    );
   }
-  // set Room as inactive //
-  deleteRoom(room: any) {
-    return this.httpClient.delete<Room>(this.urls.room.deleteRoomById(room.roomId));
+  // set Room as inactive: TO-DO //
+  deleteRoom(room: Room) {
+    return this.httpClient.delete<Room>(this.urls.room.deleteRoomById(room.roomId)).subscribe(
+      (payload) => {
+        this.room.next(payload);
+        console.log(payload);
+      }
+    );
   }
-
-  // get all Unavailabilities //
+  // get all Unavailabilities: TO-DO //
   getAllUnavailabilities() {
     return this.httpClient.get<Unavailability[]>(this.urls.unavailability.getAllUnavailabilities());
   }
-  // get Unavailability by Id //
+  // get Unavailability by Id: TO-DO //
   getOneUnavailability(unavailability: any) {
     return this.httpClient.post<Unavailability>(this.urls.unavailability.postUnavailability(), JSON.stringify(unavailability));
   }
