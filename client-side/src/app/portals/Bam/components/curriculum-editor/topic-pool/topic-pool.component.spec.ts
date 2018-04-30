7a8661f2f76e17c90d65279181aead33ab850277
Holden <tblkidhdo@yahoo.com>
2018-04-30 10:42:57 -0400

    Added unit tests for TopicPoolComponent.initTopics

--- client-side/src/app/portals/Bam/components/curriculum-editor/topic-pool/topic-pool.component.spec.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/topic-pool/topic-pool.component.spec.ts
@@ -3,8 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TopicPoolComponent } from './topic-pool.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { Dependencies } from '../../../bam.test.module';
+import { Topic } from '../../../models/topic.model';
 
-describe('TopicPoolComponent', () => {
+fdescribe('TopicPoolComponent', () => {
   let component: TopicPoolComponent;
   let fixture: ComponentFixture<TopicPoolComponent>;
 
@@ -21,4 +22,19 @@ describe('TopicPoolComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /**
+   * @author Holden Olivier
+   * @batch 1803 usf
+   */
+  it ('should set the topics array to contain the same values as the subTopicName array', () => {
+    const Topics: Array<Topic> = [{topicID: 0, topicName: 'FirstTopic'},
+      {topicID: 1, topicName: 'SecondTopic'}, {topicID: 2, topicName: 'ThirdTopic'}];
+
+    component.subTopicName = Topics;
+    component.initTopics();
+
+    expect(component.topics).toEqual(['FirstTopic', 'SecondTopic', 'ThirdTopic']);
+  });
+
 });
