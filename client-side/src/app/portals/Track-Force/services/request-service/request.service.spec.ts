8e9993c8df64ec847c1c4020866c7400959c3338
mphillipsdev <mphillips.dev@gmail.com>
2018-05-02 16:49:45 -0400

    Updating tests. -MP

--- client-side/src/app/portals/Track-Force/services/request-service/request.service.spec.ts
+++ client-side/src/app/portals/Track-Force/services/request-service/request.service.spec.ts
@@ -1,15 +1,47 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async, tick, fakeAsync, getTestBed } from '@angular/core/testing';
 
 import { RequestService } from './request.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Request } from '@angular/http';
+import { UrlService } from '../../../../hydra-client/services/urls/url.service';
 
 describe('RequestService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [RequestService]
+      providers: [RequestService, UrlService, HttpClient, HttpHandler]
     });
   });
 
   it('should be created', inject([RequestService], (service: RequestService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should return a list of marketing statuses', () => {
+    const service: RequestService = getTestBed().get(RequestService);
+    getTestBed().compileComponents().then(() => {
+
+      service.getStatuses().subscribe(res => {
+        const data = res.data;
+        console.log(data);
+        expect(data).toBeTruthy();
+        expect(data.length).toBeTruthy();
+        expect(data[0]).toBeTruthy();
+      }).unsubscribe(); // subscribe
+    }); // then
+  }); // it
+
+  it('should return a list of associates', () => {
+    const service: RequestService = getTestBed().get(RequestService);
+    getTestBed().compileComponents().then(() => {
+
+      service.getAssociates().subscribe(res => {
+        const data = res.data;
+        console.log(data);
+        expect(data).toBeTruthy();
+        expect(data.length).toBeTruthy();
+        expect(data[0]).toBeTruthy();
+      }).unsubscribe(); // subscribe
+    }); // then
+  }); // it
+
 });
