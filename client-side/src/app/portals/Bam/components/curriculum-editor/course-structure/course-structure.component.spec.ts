6e3e9e65f367918474bfcd7c4ebe579411f0b840
Shahinaz Elmahmoudi <sbloi001@fiu.edu>
2018-05-15 10:17:32 -0400

    added comments to course structure component

--- client-side/src/app/portals/Bam/components/curriculum-editor/course-structure/course-structure.component.spec.ts
+++ client-side/src/app/portals/Bam/components/curriculum-editor/course-structure/course-structure.component.spec.ts
@@ -13,16 +13,25 @@ import { SubtopicName } from '../../../models/subtopicname.model';
 import { SubtopicType } from '../../../models/subtopictype.model';
 import { TopicName } from '../../../models/topicname.model';
 describe('CourseStructureComponent', () => {
+/* Shahinaz Elmahmoudi */
+
+
+/*calling the component, the fixture and a title for testing. */
   let component: CourseStructureComponent;
   let fixture: ComponentFixture<CourseStructureComponent>;
   let curTitle: string;
+
+  // call all the dependencies
   beforeEach(async(() => {
     TestBed.configureTestingModule(Dependencies).compileComponents();
   }), 1440000);
 
+  // call the services to test
   beforeEach(() => {
     const curriculumService: CurriculumService = TestBed.get(CurriculumService);
     const subtopicService: SubtopicService = TestBed.get(SubtopicService);
+
+    // calling a function that is used in a later test and giving it data that will be tested against
     spyOn(curriculumService, 'getSchedualeByCurriculumId')
     .and
     .returnValue(Observable.of(new CurriculumSubtopic
@@ -31,6 +40,8 @@ describe('CourseStructureComponent', () => {
         (1, 'topic') , new SubtopicType
         (1, 'type')), 1, 1)));
 
+
+        // setting up a subtopic curric object
         const sub: SubtopicCurric = new SubtopicCurric();
         const topic: Topic = new Topic();
         topic.topicID = 1;
@@ -43,17 +54,21 @@ describe('CourseStructureComponent', () => {
         sub.date.endTime = 1;
         sub.date.startTime = 1;
         sub.date.week = 1;
+
+        // overriding the provider to make sure my mocks are used
     TestBed.overrideProvider(CurriculumService, {useValue: curriculumService});
     TestBed.overrideProvider(SubtopicService, {useValue: subtopicService});
+
+    // adding subtopic curric to an array
     const subArr1: Array<SubtopicCurric> = new Array();
     subArr1.push(sub);
-    // console.log('printing out the first one ');
-    // console.log(subArr1);
+
+    // spying on another function and returning the observable
     spyOn(subtopicService, 'getSubtopicByIDz')
     .and
     .callFake(function(){ return Observable.of(subArr1); });
 
-
+    // calling a function that is used in a later test and giving it data that will be tested against
     spyOn(curriculumService, 'changeData')
     .and
     .returnValue(new CurriculumSubtopic
@@ -61,16 +76,20 @@ describe('CourseStructureComponent', () => {
         (1, 'testNameChanged', new TopicName
         (1, 'topic') , new SubtopicType
         (1, 'type')), 1, 1));
+
+    // creating the component and waiting for changes
     fixture = TestBed.createComponent(CourseStructureComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
 
+  // default test
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  // checks to see if curriculums are being created. This test passes
 it(
   'CourseStructureComponent should create curriculums', async() => {
    curTitle = 'curTest';
@@ -81,6 +100,7 @@ it(
    fixture.debugElement.triggerEventHandler('messageEvent', <Event>{});
  });
 
+ // test that needs to be completed
  xit(
    'should view the curriculum schedule', async() => {
 
@@ -88,6 +108,7 @@ it(
 
    });
 
+   // test tries to use the update function but fails because it does not actually update the data
   xit(
     'should update', async() => {
 
