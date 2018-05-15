bb55d189f25667d39598f1bd4c8396f9195b08fd
Holden <tblkidhdo@yahoo.com>
2018-05-15 11:15:29 -0400

    Added comments to RemoveAssociatesFromBatchComponent. Updated removeUser test case 1 and test case 2 to check for expected parameters

--- client-side/src/app/portals/Bam/components/calendar/remove-associate-from-batch/remove-associate-from-batch.component.spec.ts
+++ client-side/src/app/portals/Bam/components/calendar/remove-associate-from-batch/remove-associate-from-batch.component.spec.ts
@@ -23,10 +23,14 @@ describe('RemoveAssociateFromBatchComponent', () => {
 
   beforeEach(() => {
 
+    // Get instances of services that need to be mocked
     const sessionService: SessionService = TestBed.get(SessionService);
     const usersService: UsersService = TestBed.get(UsersService);
 
+    // Create spies
+    // sessionService.getSelectedBatch(): Batch
     spyOn(sessionService, 'getSelectedBatch').and.returnValue(new Batch(1, null, null, null, null, 0, 0));
+    // usersService.getUsersInBatch(number): Observable<Array<BamUser>>
     spyOn(usersService, 'getUsersInBatch').and.returnValue(Observable.of(
       [
         new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
@@ -36,20 +40,20 @@ describe('RemoveAssociateFromBatchComponent', () => {
         new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
         new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
       ]));
-
-      spyOn(usersService, 'removeUserFromBatch').and.callFake((userId: number) => {
-        if (userId === 0) {
-          return Observable.of([
-            new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
-            new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
-            new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
-            new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
-            new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
-          ]);
-        } else {
-          return Observable.throw('Test');
-        }
-      });
+    // usersService.removeUserFromBatch(number): Observable<Array<BamUser>>
+    spyOn(usersService, 'removeUserFromBatch').and.callFake((userId: number) => {
+      if (userId === 0) {
+        return Observable.of([
+          new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
+          new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
+          new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
+          new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
+          new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
+        ]);
+      } else {
+        return Observable.throw('Test');
+      }
+    });
 
     fixture = TestBed.createComponent(RemoveAssociateFromBatchComponent);
     component = fixture.componentInstance;
@@ -63,32 +67,31 @@ describe('RemoveAssociateFromBatchComponent', () => {
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Testing RemoveAssociateFromBatchComponent.associateAlert()
    */
   it ('should emit an event with the type and message provided to associateAlert', (done) => {
+    // Prepare to receive event
     component.notify.subscribe((event) => {
+      // check for expectations when event is emitted
       expect(event.type).toEqual('Test');
       expect(event.message).toEqual('This is a test.');
+      // test complete
       done();
     });
 
+    // Call function
     component.associateAlert('Test', 'This is a test.');
   });
 
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Testing RemoveAssociateFromBatchComponent.removeUser() 1
+   *  Test case: Remove user at index 0
    */
   it ('should tell the usersService to remove the user with the given user\'s id from the \
   batch, then set associates to the updated user list and notify the user if successful', () => {
-    spyOn(component, 'associateAlert');
-    component.associates = [
-      new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
-    ];
+    // Generate expectations
     const expectedArr = [
       new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
       new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
@@ -97,26 +100,35 @@ describe('RemoveAssociateFromBatchComponent', () => {
       new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
     ];
 
-    component.removeUser(new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0));
+    // Create spy
+    spyOn(component, 'associateAlert');
 
-    expect(component.associateAlert).toHaveBeenCalled();
+    // Set starting values
+    component.associates = [
+      new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
+    ];
+
+    // Call function
+    component.removeUser(new BamUser(0, 'Test', null, 'User', null, null, 0, null, null, null, null, null, 0));
+
+    // Check for expectations
+    expect(component.associateAlert).toHaveBeenCalledWith('success', `Successfully removed Test User from current batch.`);
     expect(component.associates).toEqual(expectedArr);
   });
 
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Testing RemoveAssociateFromBatchComponent.removeUser() 2
+   *  Test case: Observable throws error
    */
   it ('should leave associates unchanged and alert user of failure if the given user cannot be removed from a batch', () => {
-    spyOn(component, 'associateAlert');
-    component.associates = [
-      new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
-    ];
+    // Generate expectations
     const expectedArr = [
       new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
       new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
@@ -126,26 +138,35 @@ describe('RemoveAssociateFromBatchComponent', () => {
       new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
     ];
 
-    component.removeUser(new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0));
+    // Create spy
+    spyOn(component, 'associateAlert');
 
-    expect(component.associateAlert).toHaveBeenCalled();
+    // Set starting values
+    component.associates = [
+      new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
+    ];
+
+    // Call function
+    component.removeUser(new BamUser(1, 'Test', null, 'User', null, null, 0, null, null, null, null, null, 0));
+
+    // Check for expectations
+    expect(component.associateAlert).toHaveBeenCalledWith('danger', `Error: couldn't remove Test User from current batch.`);
     expect(component.associates).toEqual(expectedArr);
   });
 
   /**
    * @author Holden Olivier
    * @batch 1803 usf
+   * Testing RemoveAssociateFromBatchComponent.removeUser() 3
+   *  Test case: Remove user not with Id not matching any user in array
    */
   it ('should do nothing if no match is found', () => {
-    spyOn(component, 'associateAlert');
-    component.associates = [
-      new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
-      new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
-    ];
+    // Generate Expectations
     const expectedArr = [
       new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
       new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
@@ -155,8 +176,23 @@ describe('RemoveAssociateFromBatchComponent', () => {
       new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
     ];
 
-    component.removeUser(new BamUser(7, null, null, null, null, null, 0, null, null, null, null, null, 0));
+    // Create spy
+    spyOn(component, 'associateAlert');
 
+    // Set starting values
+    component.associates = [
+      new BamUser(0, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(1, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(2, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(3, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(4, null, null, null, null, null, 0, null, null, null, null, null, 0),
+      new BamUser(5, null, null, null, null, null, 0, null, null, null, null, null, 0),
+    ];
+
+    // Call function
+    component.removeUser(new BamUser(7, 'Test', null, 'User', null, null, 0, null, null, null, null, null, 0));
+
+    // Check for expectations
     expect(component.associateAlert).not.toHaveBeenCalled();
     expect(component.associates).toEqual(expectedArr);
   });
