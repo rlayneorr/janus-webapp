adb2dc1843de9331801dab032688acffad8c300f
Byron Hall <byronhall020@gmail.com>
2018-05-21 16:04:40 -0400

    fixing merge conflicts

--- client-side/src/app/portals/Caliber/settings/screening/question/question.component.spec.ts
+++ client-side/src/app/portals/Caliber/settings/screening/question/question.component.spec.ts
@@ -5,35 +5,19 @@ import { QuestionComponent } from './question.component';
 import { Dependencies } from '../../../caliber.test.module';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { Question } from '../../../entities/Question';
-
-<<<<<<< HEAD
-=======
 import { Tag } from '../entities/Tag';
-import { QUESTIONS } from '../../../screening/mock-data/mock-questions';
-import { AlertService } from '../../../../../gambit-client/services/alerts/alerts.service';
-import { AlertsService } from '../../../services/alerts.service';
+import {QUESTIONS} from '../../../services/questions/mock-questions';
 
-import { Observable } from 'rxjs/Observable';
-
->>>>>>> 0063343473a6e86f9cfe6a397cd8a1bb5a546fd6
 /**
    * Last modified by the Avengers
    *
    * Byron Hall | 1803-USF-MAR26 | Wezley Singleton
-<<<<<<< HEAD
-   * 
-=======
    *
->>>>>>> 0063343473a6e86f9cfe6a397cd8a1bb5a546fd6
    * Antonio Marrero Bonilla | 1803-USF-MAR26 | Wezley Singleton
    *
    */
 
 fdescribe('QuestionComponent', () => {
-<<<<<<< HEAD
-  let component: QuestionComponent; 
-  let fixture: ComponentFixture<QuestionComponent>;  
-=======
   let component: QuestionComponent;
   let fixture: ComponentFixture<QuestionComponent>;
   const t0: Tag = new Tag();
@@ -42,7 +26,6 @@ fdescribe('QuestionComponent', () => {
   const t1: Tag = new Tag();
   t1.tagId = 2;
   t1.tagName = 'HTML';
->>>>>>> 0063343473a6e86f9cfe6a397cd8a1bb5a546fd6
 
   beforeEach(async(() => {
     TestBed.configureTestingModule(Dependencies)
@@ -64,8 +47,6 @@ fdescribe('QuestionComponent', () => {
     expect(component.sampleAnswers.length).toBe(0);
     expect(component.currentTags.length).toBe(0);
   });
-<<<<<<< HEAD
-=======
 
   it('should add new tags', () => {
     component.addNewTag(t0);
@@ -85,12 +66,4 @@ fdescribe('QuestionComponent', () => {
       component.addNewTag(t0);
       expect(component.getTagIds().length).toBe(1);
   });
-
-  xit('should change question status', () => {
-    component.changeQuesitonStatus(QUESTIONS[0]);
-    expect(QUESTIONS[0].isActive).toBe(false);
-    component.changeQuesitonStatus(QUESTIONS[0]);
-    expect(QUESTIONS[0].isActive).toBe(true);
-  });
->>>>>>> 0063343473a6e86f9cfe6a397cd8a1bb5a546fd6
 });
