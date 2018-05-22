f5d64f2c7b63b80605c22ffea7b3b1881a40a586
August Duet <august.duet@revature.com>
2018-05-22 14:04:43 -0400

    reworking integration of af

--- client-side/src/app/portals/Assign-Force/assign-force.module.ts
+++ client-side/src/app/portals/Assign-Force/assign-force.module.ts
@@ -6,11 +6,9 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { routes } from './assign-force.routes';
 import { AssignForceComponent } from './assign-force.component';
-import { HomeComponent } from './home/home.component';
 import { NavComponent } from '../../nav/nav.component';
 import { NavModule } from '../../nav/nav.module';
 import { CommonModule } from '@angular/common';
-import { CalendarComponent } from './calendar/calendar.component';
 
 
 
@@ -24,8 +22,6 @@ import { CalendarComponent } from './calendar/calendar.component';
   ],
   declarations: [
     AssignForceComponent,
-    HomeComponent,
-    CalendarComponent,
   ],
   providers: []
 })
