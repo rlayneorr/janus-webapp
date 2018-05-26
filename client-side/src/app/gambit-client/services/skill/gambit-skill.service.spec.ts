bd8360b82ab489655c2dd57426a0c2ca1892bea9
Byron Hall <byronhall020@gmail.com>
2018-05-26 18:48:57 -0400

    updated/finished gambit skill service test

--- client-side/src/app/gambit-client/services/skill/gambit-skill.service.spec.ts
+++ client-side/src/app/gambit-client/services/skill/gambit-skill.service.spec.ts
@@ -9,12 +9,8 @@ export function asyncData<T>(data: T) {
   return defer(() => Promise.resolve(data));
 }
 
-export function asyncError<T>(errorObject: any) {
-  return defer(() => Promise.reject(errorObject));
-}
-
 /**
-   * Last modified by the Avengers
+   * Test for methods on the gambit-skill service.
    *
    * Byron Hall | 1803-USF-MAR26 | Wezley Singleton
    *
@@ -22,24 +18,11 @@ export function asyncError<T>(errorObject: any) {
    *
    */
 
-fdescribe('GambitSkillService', () => {
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     imports: [
-  //       HttpClientModule
-  //     ],
-  //     providers: [
-  //       GambitSkillService,
-  //       HttpClient
-  //     ]
-  //   });
-  // });
-
-  // it('should be created', inject([GambitSkillService], (service: GambitSkillService) => {
-  //   expect(service).toBeTruthy();
-  // }));
-
+describe('GambitSkillService', () => {
   let httpClientSpyOnGet: {get: jasmine.Spy};
+  let httpClientSpyOnPost: { post: jasmine.Spy };
+  let httpClientSpyOnPut: {put: jasmine.Spy };
+  let httpClientSpyOnDelete: {delete: jasmine.Spy};
   let gambitService: GambitSkillService;
 
   const skillsArray: GambitSkill[] = [new GambitSkill(), new GambitSkill()];
@@ -51,7 +34,12 @@ fdescribe('GambitSkillService', () => {
   skillsArray[1].skillID = 2;
   skillsArray[1].skillName = 'servlet';
 
-  it('findAll should return gambitskill observable', () => {
+  /**
+   * See if findall makes an http request
+   *
+   * Function tested: findall()
+   */
+  it('findAll should return all gambitskills', () => {
     httpClientSpyOnGet = jasmine.createSpyObj('http', ['get']);
     gambitService = new GambitSkillService(<any> httpClientSpyOnGet, new UrlService);
     httpClientSpyOnGet.get.and.returnValue(asyncData(skillsArray));
@@ -60,12 +48,87 @@ fdescribe('GambitSkillService', () => {
     );
   });
 
+  /**
+   * See if findallActive makes an http request
+   *
+   * Function tested: findallActive()
+   */
   it('findAllActive should return all active gambitskills', () => {
     httpClientSpyOnGet = jasmine.createSpyObj('http', ['get']);
     gambitService = new GambitSkillService(<any> httpClientSpyOnGet, new UrlService);
     httpClientSpyOnGet.get.and.returnValue(asyncData(skillsArray));
     gambitService.findAllActive().subscribe(
       skills => expect(skills).toContain(skillsArray[1])
+    );
+  });
+
+  /**
+   * See if findById makes an http request
+   *
+   * Function tested: findById()
+   */
+  it('findById should return a gambitskill of the same id', () => {
+    httpClientSpyOnGet = jasmine.createSpyObj('http', ['get']);
+    gambitService = new GambitSkillService(<any> httpClientSpyOnGet, new UrlService);
+    httpClientSpyOnGet.get.and.returnValue(asyncData(skillsArray[0]));
+    gambitService.findById(1).subscribe(
+      skill => expect(skill).toEqual(skillsArray[0])
+    );
+  });
+
+  /**
+   * See if findByName makes an http request
+   *
+   * Function tested: findByName()
+   */
+  it('findByName should return a gambitskill with the same name', () => {
+    httpClientSpyOnGet = jasmine.createSpyObj('http', ['get']);
+    gambitService = new GambitSkillService(<any> httpClientSpyOnGet, new UrlService);
+    httpClientSpyOnGet.get.and.returnValue(asyncData(skillsArray[0]));
+    gambitService.findByName('JSP').subscribe(
+      skill => expect(skill).toEqual(skillsArray[0])
+    );
+  });
+
+  /**
+   * See if create makes an http request
+   *
+   * Function tested: create()
+   */
+  it('should create a gambitskill', () => {
+    httpClientSpyOnPost = jasmine.createSpyObj('http', ['post']);
+    gambitService = new GambitSkillService(<any> httpClientSpyOnPost, new UrlService);
+    httpClientSpyOnPost.post.and.returnValue(asyncData(skillsArray[0]));
+    gambitService.create(skillsArray[0]).subscribe(
+      skill => expect(skill).toEqual(skillsArray[0])
+    );
+  });
+
+  /**
+   * See if update makes an http request
+   *
+   * Function tested: update()
+   */
+  it('should update a gambitskill', () => {
+    httpClientSpyOnPut = jasmine.createSpyObj('http', ['put']);
+    gambitService = new GambitSkillService(<any> httpClientSpyOnPut, new UrlService);
+    httpClientSpyOnPut.put.and.returnValue(asyncData(skillsArray[0]));
+    gambitService.update(skillsArray[0]).subscribe(
+      skill => expect(skill).toEqual(skillsArray[0])
+    );
+  });
+
+  /**
+   * See if delete makes an http request
+   *
+   * Function tested: delete()
+   */
+  it('should delete a gambitskill', () => {
+    httpClientSpyOnDelete = jasmine.createSpyObj('http', ['delete']);
+    gambitService = new GambitSkillService(<any> httpClientSpyOnDelete, new UrlService);
+    httpClientSpyOnDelete.delete.and.returnValue(asyncData(true));
+    gambitService.delete(skillsArray[0]).subscribe(
+      response => expect(response).toBeTruthy()
     );
   });
 });
