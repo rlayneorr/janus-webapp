4e35a204b6f5b6312c30bbfc73da99353df0e790
goeteeks <nferman@oakland.edu>
2018-07-20 14:25:02 -0400

    added tests to (admin) skillType-buckets test suite. 3 out of 4 are passing. Need a way to send mock requests to test updating a bucket.

--- client-side/src/app/portals/Caliber/settings/screening/skillType-buckets/skillType-buckets.component.spec.ts
+++ client-side/src/app/portals/Caliber/settings/screening/skillType-buckets/skillType-buckets.component.spec.ts
@@ -1,15 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// Testing modules
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+
+// Modules
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Dependencies } from '../../../caliber.test.module';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { COMMON_DEPRECATED_DIRECTIVES } from '@angular/common/src/directives';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+// Entities
+import { SkillTypeBucket } from '../entities/SkillTypeBucket';
+import { BucketFilterPipe } from './skillType-buckets.filter';
+
+// Services
+import { AlertsService } from '../../../services/alerts.service';
+
+// Mock Data
+import { BUCKETS } from '../mock-data/mock-buckets';
+import { QuestionService } from '../../../screening/services/question/question.service';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 import { SkillTypeBucketsComponent } from './skillType-buckets.component';
+import { Question } from '../../../entities/Question';
 
-describe('SkillTypeBucketsComponent', () => {
+
+fdescribe('SkillTypeBucketsComponent', () => {
   let component: SkillTypeBucketsComponent;
   let fixture: ComponentFixture<SkillTypeBucketsComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SkillTypeBucketsComponent ]
-    })
+    TestBed.configureTestingModule(Dependencies)
     .compileComponents();
   }));
 
@@ -17,9 +37,46 @@ describe('SkillTypeBucketsComponent', () => {
     fixture = TestBed.createComponent(SkillTypeBucketsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    require('jasmine-ajax')
+
+    //jasmine.Ajax.install();
   });
 
-  it('should create', () => {
+  afterEach(function (){
+    //jasmine.Ajax.uninstall();
+  })
+
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /**
+   * Test if the bucket gets edited or not.
+   *
+   * Function Tested: editBucket()
+   **/
+  fit('should edit a bucket', () => {
+    component.editBucket(BUCKETS[0]);
+    expect(component.currBucket).toEqual(BUCKETS[0]);
+  });
+
+  /**
+   * Test if 2 buckets get sorted by isActive being true or false.
+   * 
+   * Function Tested: compare(a: Bucket, b:Bucket)
+   */
+  fit('should sort 2 buckets', ()=>{
+    let tempBucket = BUCKETS[0];
+    tempBucket.isActive = false;
+    expect(component.compare(tempBucket, BUCKETS[1])).toEqual(1);
+  });
+
+  /**
+   * Test if a bucket gets updated.
+   * 
+   * Function Tested: update(bucketParam: bucket)
+   */
+  fit('should update a bucket', ()=>{
+    expect(true).toBe(false);
+  })
 });
