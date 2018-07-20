74257c1b9a4e649c5b089ccbf29b0ca67b3d283b
goeteeks <nferman@oakland.edu>
2018-07-20 14:25:02 -0400

    added tests to (admin) skillType-buckets test suite. 3 out of 4 are passing. Need a way to send mock requests to test updating a bucket.

--- client-side/src/app/portals/Caliber/screening/components/introduction/introduction.component.spec.ts
+++ client-side/src/app/portals/Caliber/screening/components/introduction/introduction.component.spec.ts
@@ -1,4 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// Testing modules
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+
+// Modules
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Dependencies } from '../../../caliber.test.module';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { COMMON_DEPRECATED_DIRECTIVES } from '@angular/common/src/directives';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+// Entities
+import { SkillTypeBucket } from '../entities/SkillTypeBucket';
+import { BucketFilterPipe } from './skillType-buckets.filter';
+
+// Services
+import { AlertsService } from '../../../services/alerts.service';
+
+// Mock Data
+import { BUCKETS } from '../mock-data/mock-buckets';
+import { QuestionService } from '../../../screening/services/question/question.service';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 import { IntroductionComponent } from './introduction.component';
 
@@ -17,9 +37,16 @@ xdescribe('IntroductionComponent', () => {
     fixture = TestBed.createComponent(IntroductionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    require('jasmine-ajax')
+
+    //jasmine.Ajax.install();
   });
 
-  it('should create', () => {
+  afterEach(function (){
+    //jasmine.Ajax.uninstall();
+  })
+
+  fit('should create', () => {
     expect(component).toBeTruthy();
   });
 });
