9dd2f660c82c6d2331ed348c100eb55b1e33f161
Tyerra Smith <tyerra.monae@gmail.com>
2018-07-22 15:41:59 -0400

    Changed the Category entity to reflect our new ERD

--- client-side/src/app/portals/Caliber/entities/Category.ts
+++ client-side/src/app/portals/Caliber/entities/Category.ts
@@ -1,5 +1,9 @@
 // Refactored by Tyerra Smith to match the entity. A Category is essentially a skill.
 export class Category {
+<<<<<<< HEAD
     categoryId: number;
+=======
+    id: number;
+>>>>>>> Changed the Category entity to reflect our new ERD
     categoryName: string;
 };
