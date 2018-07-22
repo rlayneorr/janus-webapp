529273548083e2cd44410a589aff7621c4c0e076
Tyerra Smith <tyerra.monae@gmail.com>
2018-07-22 17:37:01 -0400

    Changed id to categoryId in the entity and the mock data - Tyerra Smith and Michael Brumley

--- client-side/src/app/portals/Caliber/entities/Category.ts
+++ client-side/src/app/portals/Caliber/entities/Category.ts
@@ -1,9 +1,13 @@
 // Refactored by Tyerra Smith to match the entity. A Category is essentially a skill.
 export class Category {
 <<<<<<< HEAD
+<<<<<<< HEAD
     categoryId: number;
 =======
     id: number;
 >>>>>>> Changed the Category entity to reflect our new ERD
+=======
+    categoryId: number;
+>>>>>>> Changed id to categoryId in the entity and the mock data - Tyerra Smith and Michael Brumley
     categoryName: string;
 };
