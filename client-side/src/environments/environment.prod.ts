a0746ec983e14d9cc9049e0af78bdb22caa06424
Tyerra Smith <tyerra.monae@gmail.com>
2018-07-24 11:18:43 -0400

    Changing context and endpoints to use mock data from JSON service for testing -Tyerra Smith and Michael Brumley

--- client-side/src/environments/environment.prod.ts
+++ client-side/src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 const context = 'http://ec2-35-182-210-106.ca-central-1.compute.amazonaws.com:10000/';
+//const context = 'http://ec2-18-216-205-109.us-east-2.compute.amazonaws.com:9999/'
 const bam = 'http://18.219.59.193:9001/api/v2';
 export const environment = {
   production: true,
   // gambitContext: 'http://ec2-35-182-210-106.ca-central-1.compute.amazonaws.com:10000/',
   localHostContext: 'http://localHostContext:3000/',
+  //caliberContext : 'http://ec2-18-216-205-109.us-east-2.compute.amazonaws.com:9999/',
   context: context, // change for what the production environment would actually be
   bam: bam,
   url: 'http://54.166.255.85:8085/',
