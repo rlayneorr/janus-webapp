308f5277ca8f7b7378b7a4c739165bcdb832b30f
santos <gabsanto94@gmail.com>
2018-07-24 18:21:00 -0400

    - change description name of suit

--- client-side/src/environments/environment.ts
+++ client-side/src/environments/environment.ts
@@ -16,8 +16,8 @@
  * @author Pedro De Los Reyes | 1803-USF-MAR26 | Wezley Singleton
  */
 
- //const context = 'http://ec2-35-182-210-106.ca-central-1.compute.amazonaws.com:10000/';
- const context = 'http://localhost:3000';
+  //const context = 'http://ec2-35-182-210-106.ca-central-1.compute.amazonaws.com:10000';
+  const context = 'http://localhost:3000';
 
   const bam = '';
 
@@ -27,6 +27,13 @@ export const environment = {
   gambitContext: 'http://ec2-35-182-210-106.ca-central-1.compute.amazonaws.com:10000',
   caliberContext: 'http://ec2-54-86-6-122.compute-1.amazonaws.com:8761',
   localhostContext: 'http://localhost:3000',
+
+  categoryContext: 'http://localhost:3001',
+  weightContext: 'http://localhost:3003',
+  bucketContext: 'http://localhost:3000',
+  skillTypeContext: 'http://localhost:3002',
+  questionsContext: 'http://localhost:3004',
+
   /** Assets endpoints for like pictures  */
   assets: 'http://52.87.205.55:8086/angular/assets/',
 
