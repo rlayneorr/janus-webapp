32c299ca8b01248cd896c339fd8cf936b225ef34
goeteeks <nferman@oakland.edu>
2018-07-24 21:20:18 -0400

    Fixed mock-data to fit with beans.

--- client-side/src/app/portals/Caliber/screening/mock-data/mock-violationTypes.ts
+++ client-side/src/app/portals/Caliber/screening/mock-data/mock-violationTypes.ts
@@ -1,8 +1,8 @@
 import { ViolationType } from '../entities/violationType';
 
 export const VIOLATION_TYPES: ViolationType[] = [
-  { violationTypeId: 11, violationType: 'Profanity' },
-  { violationTypeId: 12, violationType: 'Dress' },
-  { violationTypeId: 13, violationType: 'Attitude' },
-  { violationTypeId: 14, violationType: 'Conduct' },
+  { violationTypeId: 11, violationTypeText: 'Profanity', description: 'Use of profanity' },
+  { violationTypeId: 12, violationTypeText: 'Dress', description: 'Improper/Non-professional dress' },
+  { violationTypeId: 13, violationTypeText: 'Attitude', description: 'Poor attitude (nervousness, arrogance, etc.)' },
+  { violationTypeId: 14, violationTypeText: 'Conduct', description: 'Poor conduct (Unprofessional behavior, poor etiquette, etc.)' },
 ];
