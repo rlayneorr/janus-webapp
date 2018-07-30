aaf8c3ef023d579394c447bf0cc61e9084bc3ca3
Tyerra Smith <tyerra.monae@gmail.com>
2018-07-29 22:12:50 -0400

    Save the candidate info for the screening in local storage. -Tyerra Smith

--- client-side/src/app/portals/Caliber/screening/components/candidates-screening-list/candidates-screening-list.component.ts
+++ client-side/src/app/portals/Caliber/screening/components/candidates-screening-list/candidates-screening-list.component.ts
@@ -164,6 +164,8 @@ export class CandidatesScreeningListComponent implements OnInit {
         // take the data from the response from the database
         data => {
         // and save the screening ID as a cookie to localStorage.
+        localStorage.setItem('candidateName', this.selectedScheduledScreening.candidate.name);
+        localStorage.setItem('candidateTrack', this.selectedScheduledScreening.skillTypeId.toString());
         localStorage.setItem('screeningID', data.toString());
         console.log(localStorage.getItem('screeningID'));
       });
