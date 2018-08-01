31cf986166f904ba78ba8b07273e403cda1f5e27
Theodis Thompson <theothomp37@gmail.com>
2018-07-31 21:34:45 -0400

    ready for the big time

--- client-side/src/app/portals/Caliber/screening/services/question-score/question-score.service.spec.ts
+++ client-side/src/app/portals/Caliber/screening/services/question-score/question-score.service.spec.ts
@@ -1,24 +1,12 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
 
 import { QuestionScoreService } from './question-score.service';
 import {HttpClientModule, HttpClient} from "@angular/common/http";
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {UrlService} from "../../../../../gambit-client/services/urls/url.service";
-import {QuestionScore} from "../../entities/questionScore";
-
-
-
 
 fdescribe('QuestionScoreService', () => {
-
-  var array;
-  var update_array;
   beforeEach(() => {
-
-    array = [34, 'comment', 2, '9:30', 2];
-    update_array = [34, 'update comment', 2, '9:30',2];
-
-
     TestBed.configureTestingModule({
       imports: [HttpClientModule, HttpClientTestingModule],
       providers: [QuestionScoreService, UrlService]
@@ -28,7 +16,4 @@ fdescribe('QuestionScoreService', () => {
   it('should be created', inject([QuestionScoreService], (service: QuestionScoreService) => {
     expect(service).toBeTruthy();
   }));
-
-
-
 });
